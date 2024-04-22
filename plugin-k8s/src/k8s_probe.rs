c9505e6bac6307903b980472df8f2e1d40398e69
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@eviden.com>
2024-04-22 17:47:49 +0200

    Rebased and metrics are now registered correctly; TODO: poll function

--- plugin-k8s/src/k8s_probe.rs
+++ plugin-k8s/src/k8s_probe.rs
@@ -1,94 +1,133 @@
+use alumet::plugin::util::CounterDiffUpdate;
+use alumet::measurement::MeasurementAccumulator;
+use std::fs::File;
+use alumet::measurement::AttributeValue;
+use alumet::measurement::MeasurementPoint;
 use alumet::{
-    metrics::{AttributeValue, MeasurementPoint, TypedMetricId},
-    util::CounterDiff,
+    measurement::Timestamp,
+    metrics::TypedMetricId,
+    metrics::MetricCreationError,
+    plugin::util::CounterDiff,
+    plugin::AlumetStart,
     resources::ResourceId,
     units::Unit,
 };
+use anyhow::{Context, Result};
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+
 
 use crate::parsing_cgroupv2::{self, CgroupV2Metric};
 
-pub(crate) const PERF_MAX_ENERGY: u64 = u64::MAX;
+pub(crate) const CGROUP_MAX_TS: u64 = u64::MAX;
 
 
 /// Energy probe based on perf_event for intel RAPL.
 pub struct K8SProbe {
-    /// Id of the metric to push.
-    metric: TypedMetricId<f64>,
-    /// Ready-to-use power events with additional metadata.
-    pods: Vec<parsing_cgroupv2::CgroupV2Metric>,
+    pub name: String,
+    pub metrics: Metrics,
 }
 
-struct OpenedPowerEvent {
-    fd: File,
-    scale: f64,
-    socket: u32,
-    domain: RaplDomainType,
-    resource: ResourceId,
+#[derive(Clone)]
+pub struct Metrics {
+    pub time_used_tot: TypedMetricId<u64>,
+    pub time_used_user_mode: TypedMetricId<u64>,
+    pub time_used_system_mode: TypedMetricId<u64>,
 }
 
 impl K8SProbe {
-    pub fn new(metric: TypedMetricId<f64>, pods: Vec<parsing_cgroupv2::CgroupV2Metric>) -> anyhow::Result<K8SProbe> {
-
-        let mut opened = Vec::with_capacity(pods.len());
-        for (event, CpuId { cpu, socket }) in pods {
-            let raw_fd = event
-                .perf_event_open(pmu_type, *cpu)
-                .with_context(|| format!("perf_event_open failed. {ADVICE}"))?;
-            let fd = unsafe { File::from_raw_fd(raw_fd) };
-            let scale = event.scale as f64;
-            let opened_event = OpenedPowerEvent {
-                fd,
-                scale,
-                socket: *socket,
-                domain: event.domain,
-                resource: event.domain.to_resource(*socket),
-            };
-            let counter = CounterDiff::with_max_value(PERF_MAX_ENERGY);
-            opened.push((opened_event, pods))
-        }
-        Ok(K8SProbe { metric, pods })
+    pub fn new(metric: Metrics,name: String) -> anyhow::Result<K8SProbe> {
+        println!("New implem of K8S prob called");
+        
+        // let mut opened = Vec::with_capacity(pods.len());
+        // for (event, CpuId { cpu, socket }) in pods {
+        //     let raw_fd = event
+        //         .perf_event_open(pmu_type, *cpu)
+        //         .with_context(|| format!("perf_event_open failed. {ADVICE}"))?;
+        //     let fd = unsafe { File::from_raw_fd(raw_fd) };
+        //     let scale = event.scale as f64;
+        //     let opened_event = OpenedPowerEvent {
+        //         fd,
+        //         scale,
+        //         socket: *socket,
+        //         domain: event.domain,
+        //         resource: event.domain.to_resource(*socket),
+        //     };
+        //     let counter = CounterDiff::with_max_value(PERF_MAX_ENERGY);
+        //     opened.push((opened_event, pods))
+        // }
+        Ok(K8SProbe{
+        name: name,
+        metrics: metric,
+        })
     }
 }
 
 impl alumet::pipeline::Source for K8SProbe {
     fn poll(
         &mut self,
-        measurements: &mut alumet::metrics::MeasurementAccumulator,
-        timestamp: std::time::SystemTime,
+        measurements: &mut MeasurementAccumulator,
+        timestamp: Timestamp,
     ) -> Result<(), alumet::pipeline::PollError> {
-        for (evt, counter) in &mut self.events {
-            // read the new value of the perf-events counter
-            let counter_value = read_perf_event(&mut evt.fd)
-                .with_context(|| format!("failed to read perf_event {:?} for domain {:?}", evt.fd, evt.domain))?;
+        // for (evt, counter) in &mut self.events {
+        //     // read the new value of the perf-events counter
+        //     let counter_value = read_perf_event(&mut evt.fd)
+        //         .with_context(|| format!("failed to read perf_event {:?} for domain {:?}", evt.fd, evt.domain))?;
 
-            // correct any overflows
-            let diff = match counter.update(counter_value) {
-                alumet::util::CounterDiffUpdate::FirstTime => None,
-                alumet::util::CounterDiffUpdate::Difference(diff) => Some(diff),
-                alumet::util::CounterDiffUpdate::CorrectedDifference(diff) => {
-                    log::debug!("Overflow on perf_event counter for RAPL domain {}", evt.domain);
-                    Some(diff)
-                }
-            };
-            if let Some(value) = diff {
-                // convert to joules and push
-                let joules = (value as f64) * evt.scale;
-                measurements.push(MeasurementPoint::new(
-                    timestamp,
-                    self.metric,
-                    evt.resource.clone(),
-                    joules,
-                ).with_attr("domain", AttributeValue::String(evt.domain.to_string())));
-            }
-            // NOTE: the energy can be a floating-point number in Joules,
-            // without any loss of precision. Why? Because multiplying any number
-            // by a float that is a power of two will only change the "exponent" part,
-            // not the "mantissa", and the energy unit for RAPL is always a power of two.
-            //
-            // A f32 can hold integers without any precision loss
-            // up to approximately 2^24, which is not enough for the RAPL counter values,
-            // so we use a f64 here.
-        }
+        //     // correct any overflows
+        //     let diff = match counter.update(counter_value) {
+        //         alumet::util::CounterDiffUpdate::FirstTime => None,
+        //         alumet::util::CounterDiffUpdate::Difference(diff) => Some(diff),
+        //         alumet::util::CounterDiffUpdate::CorrectedDifference(diff) => {
+        //             log::debug!("Overflow on perf_event counter for RAPL domain {}", evt.domain);
+        //             Some(diff)
+        //         }
+        //     };
+        //     if let Some(value) = diff {
+        //         // convert to joules and push
+        //         let joules = (value as f64) * evt.scale;
+        //         measurements.push(MeasurementPoint::new(
+        //             timestamp,
+        //             self.metric,
+        //             evt.resource.clone(),
+        //             joules,
+        //         ).with_attr("domain", AttributeValue::String(evt.domain.to_string())));
+        //     }
+        //     // NOTE: the energy can be a floating-point number in Joules,
+        //     // without any loss of precision. Why? Because multiplying any number
+        //     // by a float that is a power of two will only change the "exponent" part,
+        //     // not the "mantissa", and the energy unit for RAPL is always a power of two.
+        //     //
+        //     // A f32 can hold integers without any precision loss
+        //     // up to approximately 2^24, which is not enough for the RAPL counter values,
+        //     // so we use a f64 here.
+        // }
+        // for metric in 
+
+        let since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards");
+        println!("Poll Called at {:?}", since_the_epoch); 
+        // let mut rng = rand::thread_rng();
+        // let final_value: f32 = (rng.gen::<f32>())*100.0;        
+        // let p1: MeasurementPoint = MeasurementPoint::new(timestamp, self.energy_a, self.resource.clone(), final_value.into());
+        // let p2: MeasurementPoint = MeasurementPoint::new(timestamp, self.temperature_a, self.resource.clone(), final_value.into());
+        // measurements.push(p1);
+        // measurements.push(p2);
+
         Ok(())
+    }
+}
+
+
+impl Metrics {
+    pub fn new(alumet: &mut AlumetStart) -> Result<Self, MetricCreationError> {
+        let usec = Unit::Custom {
+            unique_name: "usec".to_owned(),
+            display_name: "µsec".to_owned(),
+        };
+        Ok(Self {
+        time_used_tot: alumet.create_metric::<u64>("total_usage_usec", usec.clone(), "Total CPU usage time by the group")?, 
+        time_used_user_mode: alumet.create_metric::<u64>("user_usage_usec", usec.clone(), "User CPU usage time by the group")?,
+        time_used_system_mode: alumet.create_metric::<u64>("system_usage_usec", usec.clone(), "System CPU usage time by the group")?,
+        })
     }
 }
--- plugin-k8s/src/lib.rs
+++ plugin-k8s/src/lib.rs
@@ -1,25 +1,34 @@
 use std::collections::HashMap;
-use alumet::units::Unit;
+use std::time::Duration;
+use alumet::{pipeline::{trigger::Trigger, Source}, plugin::{rust::AlumetPlugin, ConfigTable}, units::Unit};
 
 use anyhow::{Error, anyhow};
 
-pub struct K8sPlugin;
+pub struct K8sPlugin{
+    poll_interval: Duration,
+}
 
 mod cgroup_v2;
 mod parsing_cgroupv2;
 mod k8s_probe;
 
-use crate::parsing_cgroupv2::CgroupV2Metric;
+use crate::{parsing_cgroupv2::CgroupV2Metric};
 use crate::cgroup_v2::CgroupV2MetricFile;
-use crate::k8s_probe::K8SProbe;
+use crate::k8s_probe::{K8SProbe, Metrics};
 
-impl alumet::plugin::Plugin for K8sPlugin{
-    fn name(&self) -> &str {
+impl AlumetPlugin for K8sPlugin{
+    fn name() -> &'static str {
         "K8S"
     }
 
-    fn version(&self) -> &str {
+    fn version() -> &'static str {
         "0.0.1"
+    }
+
+    fn init(_config: ConfigTable) -> anyhow::Result<Box<Self>> {
+        // TODO read from config
+        let poll_interval = Duration::from_secs(1);
+        Ok(Box::new(K8sPlugin { poll_interval }))
     }
 
     fn start(&mut self, alumet: &mut alumet::plugin::AlumetStart) -> anyhow::Result<()> {
@@ -38,36 +47,48 @@ impl alumet::plugin::Plugin for K8sPlugin{
             let first_metrics: Vec<CgroupV2Metric> = cgroup_v2::gather_value(&final_li_metric_file);
 
             // I suppose value are 64 bit long
-            let usec = Unit::CustomUnit{
-                unique_name: "usec",
-                display_name: "µsec",
-                debug_name: "µsec",
-            };
-            let metric = alumet.create_metric::<u64>("total_usage_usec", Unit::Custom(usec), "Total CPU usage time by the group")?;
-            let metric = alumet.create_metric::<u64>("user_usage_usec", Unit::Custom(usec), "User CPU usage time by the group")?;
-            let metric = alumet.create_metric::<u64>("system_usage_usec", Unit::Custom(usec), "System CPU usage time by the group")?;
-            let mut events_on_cpus = Vec::new();
-            for event in &available_domains.perf_events {
-                for cpu in &socket_cpus {
-                    events_on_cpus.push((event, cpu));
-                }
+            
+            let metrics_result = k8s_probe::Metrics::new(alumet);
+            // let metric_tot = alumet.create_metric::<u64>("total_usage_usec", usec.clone(), "Total CPU usage time by the group")?;
+            // let metric_usr = alumet.create_metric::<u64>("user_usage_usec", usec.clone(), "User CPU usage time by the group")?;
+            // let metric_sys = alumet.create_metric::<u64>("system_usage_usec", usec.clone(), "System CPU usage time by the group")?;
+            // let metric = CgroupV2Metric {
+            //     time_used_tot: metric_tot,
+            //     time_used_user_mode: metric_usr,
+            //     time_used_system_mode: metric_sys,
+            // };
+            // let mut events_on_cpus: Vec<CgroupV2Metric> = Vec::new();
+            // for event in &available_domains.perf_events {
+            //     for cpu in &socket_cpus {
+            //         events_on_cpus.push((event, cpu));
+            //     }
+            // }
+            // log::debug!("Events to read: {events_on_cpus:?}");
+            match metrics_result {
+                Ok(metrics) => {
+                    for pod in first_metrics {
+                        let probe = K8SProbe::new(metrics.clone(), pod.name)?;
+                        alumet.add_source(Box::new(probe), Trigger::at_interval(self.poll_interval));  
+                        println!("\n------------------------------------\n");  
+                    }
+                },
+                Err(_) => todo!()
             }
-            log::debug!("Events to read: {events_on_cpus:?}");
-            let probe = PerfEventProbe::new(metric, &events_on_cpus)?;
-            alumet.add_source(Box::new(probe));
-            Ok(())
+            
+            
+            return Ok(());
     
 
 
             // for elem in &first_metrics{
             //     println!("{:?}", elem);
             // }
-            cgroup_v2::diff_value(&mut all_value_saved, first_metrics);
-            for i in 1..10{
-                let tmpVec: Vec<CgroupV2Metric> = cgroup_v2::gather_value(&final_li_metric_file);
-                cgroup_v2::diff_value(&mut all_value_saved, tmpVec);
-                println!("\n------------------------------------\n");
-            }
+            // cgroup_v2::diff_value(&mut all_value_saved, first_metrics);
+            // for i in 1..10{
+            //     let tmpVec: Vec<CgroupV2Metric> = cgroup_v2::gather_value(&final_li_metric_file);
+            //     cgroup_v2::diff_value(&mut all_value_saved, tmpVec);
+            //     println!("\n------------------------------------\n");
+            // }
 
         }else{
             panic!("Cgroups v2 are not being used!");
