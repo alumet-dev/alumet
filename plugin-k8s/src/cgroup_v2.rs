8d5c8c27dc44780bf878258d3566158165484390
Cyprien <74776317+AngeC15@users.noreply.github.com>
2024-05-24 15:17:17 +0200

    Discovering folders for pods (#3)
    * commit to discovert folders for pods
    
    * Modified structures to use path instead of String

--- plugin-k8s/src/cgroup_v2.rs
+++ plugin-k8s/src/cgroup_v2.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{self, File},
-    io::{self, Error, Read, Seek},
+    io::{Read, Seek},
     path::{Path, PathBuf},
     result::Result::Ok,
     str::FromStr,
@@ -40,48 +40,25 @@ pub fn is_accessible_dir(path: &Path) -> bool {
     path.is_dir()
 }
 
-/// Retrieves the name of a cgroup without its prefix.
-///
-/// ## Example
-/// ```ignore
-/// use std::path::PathBuf;
-/// 
-/// let path = PathBuf::from("myPath/kubepods-burstable.slice/kubepods-burstable-podABCD");
-/// assert_eq!(retrieve_name(&path, &"kubepods-burstable-".to_string()).unwrap(),"podABCD");
-/// ```
-fn retrieve_name(path: &Path, prefix: &str) -> anyhow::Result<String> {
-    // Get the last component of the path (file or directory name)
-    if path.as_os_str().is_empty() {
-        anyhow::bail!("Path can't be empty")
-    }
-    if prefix != "" {
-        let file_name = path.file_name().unwrap().to_string_lossy();
-        let file_strip_prefix = file_name.strip_prefix(prefix).unwrap_or(&file_name);
-        let file_strip_suffix = file_strip_prefix.strip_suffix(&".slice").unwrap_or(file_strip_prefix);
-        return Ok(file_strip_suffix.to_owned());
-    }
-    Ok("".to_owned())
-}
 
 /// Returns a Vector of CgroupV2MetricFile associated to pods availables under a given directory.
-fn list_metric_file_in_dir(root_directory_path: &String, prefix: &String) -> anyhow::Result<Vec<CgroupV2MetricFile>> {
-    let root_path = Path::new(root_directory_path);
-    let prefix_path = Path::new(prefix);
-    let dir = root_path.join(prefix_path);
+fn list_metric_file_in_dir(root_directory_path: &Path) -> anyhow::Result<Vec<CgroupV2MetricFile>> {
     let mut vec_file_metric: Vec<CgroupV2MetricFile> = Vec::new();
-    let entries = fs::read_dir(dir)?;
+    let entries = fs::read_dir(root_directory_path)?;
     for entry in entries {
-        let mut path = entry?.path();
+        let path = entry?.path();
+        let mut path_cloned = path.clone();
         if path.is_dir() {
-            let _dir_name = path.file_name().expect("Impossible to write dir name");
-            let truncated_prefix = prefix.strip_suffix(".slice/").unwrap_or(&prefix);
-            let mut new_prefix = truncated_prefix.to_owned();
-            new_prefix.push_str("-");
-            let name = retrieve_name(&path, &new_prefix.to_owned())?;
-            path.push("cpu.stat");
-            let file = File::open(&path).with_context(|| format!("failed to open file {}", path.display()))?;
+            let dir_name = path.file_name().unwrap().to_str().with_context(|| format!("filename is not valid UTF-8: {path:?}"))?;
+            let dir_name_mod = dir_name.strip_suffix(".slice").unwrap_or(&dir_name);
+            let truncated_prefix = root_directory_path.file_name().unwrap().to_str().with_context(|| format!("filename is not valid UTF-8: {path:?}"))?;
+            let mut new_prefix = truncated_prefix.strip_suffix(".slice").unwrap_or(&truncated_prefix).to_owned();
+            new_prefix.push_str("-");            
+            let name = dir_name_mod.strip_prefix(&new_prefix).unwrap_or(&dir_name_mod);
+            path_cloned.push("cpu.stat");
+            let file = File::open(&path_cloned).with_context(|| format!("failed to open file {}", path_cloned.display()))?;
             vec_file_metric.push(CgroupV2MetricFile {
-                name: name,
+                name: name.to_owned(),
                 path: path,
                 file: file,
             });
@@ -93,19 +70,26 @@ fn list_metric_file_in_dir(root_directory_path: &String, prefix: &String) -> any
 /// This function list all k8s pods availables, using 3 sub-directory to look in:
 /// For each subdirectory, we look in if there is a directory/ies about pods and we add it
 /// to a vector. All subdirectory are visited with the help of <list_metric_file_in_dir> function.
-pub fn list_all_k8s_pods_file() -> anyhow::Result<Vec<CgroupV2MetricFile>> {
+pub fn list_all_k8s_pods_file(root_directory_str: &str) -> anyhow::Result<Vec<CgroupV2MetricFile>> {
     let mut final_li_metric_file: Vec<CgroupV2MetricFile> = Vec::new();
-    let root_directory_path: &str = "/sys/fs/cgroup/kubepods.slice/";
-    if !Path::new(root_directory_path).exists() {
+    let root_directory_path = Path::new(root_directory_str);
+    if !root_directory_path.exists() {
         return Ok(final_li_metric_file);
     }
-    let all_sub_dir: Vec<String> = vec![
-        "".to_string(),
-        "kubepods-besteffort.slice/".to_string(),
-        "kubepods-burstable.slice/".to_string(),
-    ];
+    // Add the root for all subdirectory:
+    let mut all_sub_dir: Vec<PathBuf>  = vec![root_directory_path.to_owned()];
+    // Iterate in the root directory and add to the vec all folder ending with "".slice"
+    // On unix, folders are files, files are files and peripherals are also files
+    for file in fs::read_dir(root_directory_str)?{
+        let path = file?.path();
+        let file_name = path.file_name().unwrap().to_str().with_context(|| format!("filename is not valid UTF-8: {path:?}"))?;
+        if path.is_dir() && file_name.ends_with(".slice"){
+            all_sub_dir.push(path);            
+        }
+    }
+
     for prefix in all_sub_dir {
-        let mut result_vec = list_metric_file_in_dir(&root_directory_path.to_owned(), &prefix.to_owned())?;
+        let mut result_vec = list_metric_file_in_dir(&prefix.to_owned())?;
         final_li_metric_file.append(&mut result_vec);
     }
     return Ok(final_li_metric_file);
@@ -144,51 +128,6 @@ mod tests {
     }
 
     #[test]
-    fn test_retrieve_name() {
-        let tmp = std::env::temp_dir();
-        let root: std::path::PathBuf = tmp.join("test-alumet-plugin-k8s/kubepods-besteffort.slice");
-        if root.exists() {
-            std::fs::remove_dir_all(&root).unwrap();
-        }
-        let burstable_dir = root.join("kubepods-burstable.slice");
-        let besteffort_dir = root.join("kubepods-besteffort.slice");
-        std::fs::create_dir_all(&burstable_dir).unwrap();
-        std::fs::create_dir_all(&besteffort_dir).unwrap();
-
-        let a = burstable_dir.join("kubepods-burstable-pod32a1942cb9a81912549c152a49b5f9b1.slice");
-        let b = burstable_dir.join("kubepods-besteffort-podd9209de2b4b526361248c9dcf3e702c0.slice");
-        let c = besteffort_dir.join("kubepods-besteffort-pod32a1942cb9a81912549c152a49b5f9b1.slice");
-        let d = besteffort_dir.join("kubepods-burstable-podd9209de2b4b526361248c9dcf3e702c0.slice");
-        std::fs::create_dir_all(&a).unwrap();
-        std::fs::create_dir_all(&b).unwrap();
-        std::fs::create_dir_all(&c).unwrap();
-        std::fs::create_dir_all(&d).unwrap();
-
-        assert_eq!(
-            retrieve_name(&a, &"kubepods-burstable-".to_string()).unwrap(),
-            "pod32a1942cb9a81912549c152a49b5f9b1"
-        );
-        assert_eq!(
-            retrieve_name(&b, &"kubepods-burstable-".to_string()).unwrap(),
-            "kubepods-besteffort-podd9209de2b4b526361248c9dcf3e702c0"
-        );
-        assert_eq!(
-            retrieve_name(&c, &"kubepods-besteffort-".to_string()).unwrap(),
-            "pod32a1942cb9a81912549c152a49b5f9b1"
-        );
-        assert_eq!(
-            retrieve_name(&d, &"kubepods-besteffort-".to_string()).unwrap(),
-            "kubepods-burstable-podd9209de2b4b526361248c9dcf3e702c0"
-        );
-
-        let path_buf = PathBuf::from("");
-        let name = "zkjbf".to_string();
-        match retrieve_name(path_buf.as_path(), &name) {
-            Ok(_) => assert!(false),
-            Err(_) => assert!(true),
-        };
-    }
-    #[test]
     fn test_list_metric_file_in_dir() {
         let tmp = std::env::temp_dir();
         let root: std::path::PathBuf = tmp.join("test-alumet-plugin-k8s/kubepods-folder.slice/");
@@ -210,10 +149,7 @@ mod tests {
         std::fs::write(b.join("cpu.stat"), "fr").unwrap();
         std::fs::write(c.join("cpu.stat"), "sv").unwrap();
         std::fs::write(d.join("cpu.stat"), "ne").unwrap();
-        let li_met_file: anyhow::Result<Vec<CgroupV2MetricFile>> = list_metric_file_in_dir(
-            &root.into_os_string().into_string().unwrap(),
-            &"kubepods-burstable.slice/".to_owned(),
-        );
+        let li_met_file: anyhow::Result<Vec<CgroupV2MetricFile>> = list_metric_file_in_dir(&burstable_dir);
         let list_pod_name = [
             "pod32a1942cb9a81912549c152a49b5f9b1",
             "podd9209de2b4b526361248c9dcf3e702c0",
