19928629d84f9d705b75abf3ec0d1fd82c47427c
sofiaduqueg <s.duqueg@uniandes.edu.co>
2024-05-28 14:17:45 +0200

    Oar Plugin compiles and runs correctly

--- plugin-oar2/src/lib.rs
+++ plugin-oar2/src/lib.rs
@@ -1,22 +1,26 @@
-use std::{
-    fs::{self, File}, io::{Read, Seek}, mem, path::PathBuf, sync::Arc, time::Duration
-};
 use alumet::{
-    metrics::TypedMetricId, 
-    pipeline::{runtime::ControlHandle, trigger::TriggerSpec, Source, PollError}, 
-    plugin::{rust::{deserialize_config, AlumetPlugin, serialize_config}, AlumetStart, ConfigTable, Plugin}, 
+    measurement::{MeasurementAccumulator, MeasurementPoint, Timestamp},
+    metrics::TypedMetricId,
+    pipeline::{runtime::ControlHandle, trigger::TriggerSpec, PollError, Source},
+    plugin::{
+        rust::{deserialize_config, serialize_config, AlumetPlugin},
+        AlumetStart, ConfigTable, Plugin,
+    },
     resources::{Resource, ResourceConsumer},
-    measurement::{MeasurementAccumulator, Timestamp, MeasurementPoint}, 
-    units::{PrefixedUnit, Unit}};
-use notify::{recommended_watcher, Event, EventHandler, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
-use serde::{Deserialize, Serialize};
+    units::{PrefixedUnit, Unit},
+};
 use anyhow::Context;
-use log::trace;
+use notify::{Event, EventHandler, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use serde::{Deserialize, Serialize};
+use std::{
+    ffi::OsStr, fs::{self, File}, io::{Read, Seek}, path::PathBuf, time::Duration
+};
 
 #[derive(Debug)]
 pub struct Oar2Plugin {
     config: Config,
     metrics: Option<Metrics>,
+    watcher: Option<RecommendedWatcher>,
 }
 #[derive(Debug, Clone)]
 pub struct Metrics {
@@ -32,11 +36,14 @@ struct Config {
 }
 
 #[derive(Debug)]
-struct OarJobSource{
+struct OarJobSource {
     cpu_metric: TypedMetricId<u64>,
     memory_metric: TypedMetricId<u64>,
     cgroup_cpu_file: File,
     cgroup_memory_file: File,
+    cpu_file_path: PathBuf,
+    memory_file_path: PathBuf,
+    job_id: u64,
 }
 
 impl AlumetPlugin for Oar2Plugin {
@@ -54,14 +61,14 @@ impl AlumetPlugin for Oar2Plugin {
 
     fn init(config: ConfigTable) -> anyhow::Result<Box<Self>> {
         let config: Config = deserialize_config(config)?;
-        Ok(Box::new(Oar2Plugin { config, metrics : None }))
+        Ok(Box::new(Oar2Plugin {
+            config,
+            metrics: None,
+            watcher: None,
+        }))
     }
 
     fn start(&mut self, alumet: &mut AlumetStart) -> Result<(), anyhow::Error> {
-
-        // When creating the metric, should the metric id include the job id? -> no
-        // Should be stored in the plugin for use in post_pipeline_start.
-
         let cpu_metric = alumet.create_metric::<u64>(
             "cpu_time",
             PrefixedUnit::nano(Unit::Second),
@@ -73,16 +80,28 @@ impl AlumetPlugin for Oar2Plugin {
             "Total memory usage by the cgroup (in bytes).",
         )?;
 
-        self.metrics = Some(Metrics { cpu_metric, memory_metric });
+        self.metrics = Some(Metrics {
+            cpu_metric,
+            memory_metric,
+        });
 
         let cgroup_cpu_path = self.config.path.join("cpuacct/oar");
         let cgroup_memory_path = self.config.path.join("memory/oar");
 
+
         // Scanning to check if there are jobs already running
-        for entry in fs::read_dir(&cgroup_cpu_path).with_context(|| format!("cgroup cpu path is, my_var={cgroup_cpu_path:?}"))? {
+        for entry in
+            fs::read_dir(&cgroup_cpu_path).with_context(|| format!("cgroup cpu path is, my_var={cgroup_cpu_path:?}"))?
+        {
             let entry = entry?;
+
             let job_name = entry.file_name();
-            if entry.file_type()?.is_dir() && job_name.to_string_lossy().chars().any(|c| c.is_numeric()) {
+            let job_name = job_name.into_string().unwrap();
+        
+            if entry.file_type()?.is_dir() && job_name.chars().any(|c| c.is_numeric()) {
+                let job_separated = job_name.split_once("_");
+                let job_id = job_separated.context("Invalid oar cgroup.")?.1.parse()?;
+
                 let cpu_job_path = cgroup_cpu_path.join(&job_name);
                 let memory_job_path = cgroup_memory_path.join(&job_name);
 
@@ -99,6 +118,9 @@ impl AlumetPlugin for Oar2Plugin {
                     memory_metric,
                     cgroup_cpu_file,
                     cgroup_memory_file,
+                    cpu_file_path,
+                    memory_file_path,
+                    job_id,
                 });
 
                 alumet.add_source(initial_source, TriggerSpec::at_interval(self.config.poll_interval));
@@ -117,7 +139,7 @@ impl AlumetPlugin for Oar2Plugin {
         let memory_metric = metrics.memory_metric;
         let poll_interval = self.config.poll_interval;
 
-        struct JobDetector{
+        struct JobDetector {
             config_path: PathBuf,
             cpu_metric: TypedMetricId<u64>,
             memory_metric: TypedMetricId<u64>,
@@ -128,27 +150,50 @@ impl AlumetPlugin for Oar2Plugin {
 
         impl EventHandler for JobDetector {
             fn handle_event(&mut self, event: Result<Event, notify::Error>) {
-                if let Ok(Event { kind: EventKind::Create(_), paths, .. }) = event {
+                log::debug!("Handle event function");
+                if let Ok(Event {
+                    kind: EventKind::Create(_),
+                    paths,
+                    ..
+                }) = event
+                {
+                    log::debug!("Paths: {paths:?}");
                     for path in paths {
                         if let Some(job_name) = path.file_name() {
-                            if job_name.to_string_lossy().chars().any(|c| c.is_numeric()) {
+                            let job_name = job_name.to_str().unwrap();
+                            if job_name.chars().any(|c| c.is_numeric()) {
+                                let job_separated = job_name.split_once("_");
+                                let job_id = job_separated.context("Invalid oar cgroup").unwrap().1.parse().unwrap();
                                 let cpu_path = self.config_path.join("cpuacct/oar").join(&job_name);
+                                log::debug!("CPU path {cpu_path:?}");
                                 let memory_path = self.config_path.join("memory/oar").join(&job_name);
+                                log::debug!("Memory path {memory_path:?}");
                                 let cpu_file_path = cpu_path.join("cpuacct.usage");
+                                log::debug!("CPU file path {cpu_file_path:?}");
                                 let memory_file_path = memory_path.join("memory.usage_in_bytes");
-            
-                                if let (Ok(cgroup_cpu_file), Ok(cgroup_memory_file)) = (File::open(&cpu_file_path), File::open(&memory_file_path)) {
-                                    
+                                log::debug!("Memory file path {memory_file_path:?}");
+
+                                if let (Ok(cgroup_cpu_file), Ok(cgroup_memory_file)) =
+                                    (File::open(&cpu_file_path), File::open(&memory_file_path))
+                                {
                                     let new_source = Box::new(OarJobSource {
                                         cpu_metric: self.cpu_metric,
                                         memory_metric: self.memory_metric,
                                         cgroup_cpu_file,
                                         cgroup_memory_file,
+                                        cpu_file_path,
+                                        memory_file_path,
+                                        job_id,
                                     });
-        
-                                    let source_name = job_name.to_string_lossy().to_string();
-            
-                                    self.control_handle.add_source(self.plugin_name.clone(), source_name, new_source, TriggerSpec::at_interval(self.poll_interval));
+
+                                    let source_name = job_name.to_string();
+
+                                    self.control_handle.add_source(
+                                        self.plugin_name.clone(),
+                                        source_name,
+                                        new_source,
+                                        TriggerSpec::at_interval(self.poll_interval),
+                                    );
                                 }
                             }
                         }
@@ -158,12 +203,21 @@ impl AlumetPlugin for Oar2Plugin {
                 }
             }
         }
-            
-        let handler = JobDetector { config_path: config_path.clone(), cpu_metric, memory_metric, control_handle, plugin_name, poll_interval };
+
+        let handler = JobDetector {
+            config_path: config_path.clone(),
+            cpu_metric,
+            memory_metric,
+            control_handle,
+            plugin_name,
+            poll_interval,
+        };
         let mut watcher = notify::recommended_watcher(handler)?;
-            
+
         watcher.watch(&config_path.join("cpuacct/oar"), RecursiveMode::NonRecursive)?;
         watcher.watch(&config_path.join("memory/oar"), RecursiveMode::NonRecursive)?;
+
+        self.watcher = Some(watcher);
 
         Ok(())
     }
@@ -186,7 +240,7 @@ impl Default for Config {
 
 impl Source for OarJobSource {
     fn poll(&mut self, measurements: &mut MeasurementAccumulator, timestamp: Timestamp) -> Result<(), PollError> {
-        log::debug!("OarJobSource::poll at {timestamp:?}");
+        // log::debug!("OarJobSource::poll at {timestamp:?}");
         let cpu_usage_file = &mut self.cgroup_cpu_file;
         cpu_usage_file.rewind()?;
         let mut cpu_usage = String::new();
@@ -200,21 +254,30 @@ impl Source for OarJobSource {
 
         //Resource consumer: whole path to the job
 
-        measurements.push(MeasurementPoint::new(
-            timestamp,
-            self.cpu_metric,
-            Resource::LocalMachine,
-            ResourceConsumer::LocalMachine,
-            cpu_usage_u64,
-        )/*.with_attr("oar_job_id", the_job_id)*/);
+        measurements.push(
+            MeasurementPoint::new(
+                timestamp,
+                self.cpu_metric,
+                Resource::LocalMachine,
+                ResourceConsumer::ControlGroup {
+                    path: (self.cpu_file_path.to_str().unwrap().to_owned().into()),
+                },
+                cpu_usage_u64,
+            )
+            .with_attr("oar_job_id", self.job_id),
+        );
 
         measurements.push(MeasurementPoint::new(
             timestamp,
             self.memory_metric,
             Resource::LocalMachine,
-            ResourceConsumer::LocalMachine,
+            ResourceConsumer::ControlGroup {
+                path: (self.memory_file_path.to_str().unwrap().to_owned().into()),
+            },
             memory_usage_u64,
-        ));
+            )
+            .with_attr("oar_job_id", self.job_id),
+        );
 
         Ok(())
     }
