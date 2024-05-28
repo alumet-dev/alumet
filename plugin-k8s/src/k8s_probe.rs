d1f157292512495c72d6062b5ebeae15b2858a0a
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@eviden.com>
2024-05-28 15:04:32 +0200

    Added a way to watch creation and deletion of pods

--- plugin-k8s/src/k8s_probe.rs
+++ plugin-k8s/src/k8s_probe.rs
@@ -9,9 +9,13 @@ use alumet::{
     units::{PrefixedUnit, Unit},
 };
 use anyhow::Result;
+use inotify::EventMask;
+use std::{fs::File, path::PathBuf};
 
 use crate::cgroup_v2::{self, CgroupV2MetricFile};
-use crate::parsing_cgroupv2::CgroupV2Metric;
+
+use super::INOTIFY_VAR;
+use super::MAP_FD;
 
 pub(crate) const CGROUP_MAX_TIME_COUNTER: u64 = u64::MAX;
 
@@ -38,11 +42,21 @@ impl K8SProbe {
             let counter_tmp_sys = CounterDiff::with_max_value(CGROUP_MAX_TIME_COUNTER);
             metric_counter.push((metric_file, counter_tmp_tot, counter_tmp_usr, counter_tmp_sys));
         }
+
+
         return Ok(K8SProbe {
             metrics: metric,
             metric_and_counter: metric_counter,
         });
     }
+
+    pub fn add_entry(&mut self, metric_file: CgroupV2MetricFile) {
+        let counter_tmp_tot = CounterDiff::with_max_value(CGROUP_MAX_TIME_COUNTER);
+        let counter_tmp_usr = CounterDiff::with_max_value(CGROUP_MAX_TIME_COUNTER);
+        let counter_tmp_sys = CounterDiff::with_max_value(CGROUP_MAX_TIME_COUNTER);
+        self.metric_and_counter.push((metric_file, counter_tmp_tot, counter_tmp_usr, counter_tmp_sys));
+    }
+
 }
 
 impl alumet::pipeline::Source for K8SProbe {
@@ -52,57 +66,115 @@ impl alumet::pipeline::Source for K8SProbe {
         timestamp: Timestamp,
     ) -> Result<(), alumet::pipeline::PollError> {
         let mut file_buffer = String::new();
+        let mut element_unreachable: Vec<String> = Vec::new();
         for (metric_file, counter_tot, counter_usr, counter_sys) in &mut self.metric_and_counter {
-            let metrics: CgroupV2Metric = cgroup_v2::gather_value(metric_file, &mut file_buffer)?;
-            let diff_tot = match counter_tot.update(metrics.time_used_tot) {
-                CounterDiffUpdate::FirstTime => None,
-                CounterDiffUpdate::Difference(diff) | CounterDiffUpdate::CorrectedDifference(diff) => Some(diff),
+            match cgroup_v2::gather_value(metric_file, &mut file_buffer){
+                Ok(metrics_gathered) => {
+                    let diff_tot = match counter_tot.update(metrics_gathered.time_used_tot) {
+                        CounterDiffUpdate::FirstTime => None,
+                        CounterDiffUpdate::Difference(diff) | CounterDiffUpdate::CorrectedDifference(diff) => Some(diff),
+                    };
+                    let diff_usr = match counter_usr.update(metrics_gathered.time_used_user_mode) {
+                        CounterDiffUpdate::FirstTime => None,
+                        CounterDiffUpdate::Difference(diff) => Some(diff),
+                        CounterDiffUpdate::CorrectedDifference(diff) => Some(diff),
+                    };
+                    let diff_sys = match counter_sys.update(metrics_gathered.time_used_system_mode) {
+                        CounterDiffUpdate::FirstTime => None,
+                        CounterDiffUpdate::Difference(diff) => Some(diff),
+                        CounterDiffUpdate::CorrectedDifference(diff) => Some(diff),
+                    };
+                    let consumer = ResourceConsumer::ControlGroup {
+                        path: (metric_file.path.to_string_lossy().to_string().into()),
+                    };
+                    if let Some(value_tot) = diff_tot {
+                        let p_tot: MeasurementPoint = MeasurementPoint::new(
+                            timestamp,
+                            self.metrics.time_used_tot,
+                            Resource::LocalMachine,
+                            consumer.clone(),
+                            value_tot as u64,
+                        )
+                        .with_attr("pod", AttributeValue::String(metrics_gathered.name.clone()));
+                        measurements.push(p_tot);
+                    }
+                    if let Some(value_usr) = diff_usr {
+                        let p_usr: MeasurementPoint = MeasurementPoint::new(
+                            timestamp,
+                            self.metrics.time_used_user_mode,
+                            Resource::LocalMachine,
+                            consumer.clone(),
+                            value_usr as u64,
+                        )
+                        .with_attr("pod", AttributeValue::String(metrics_gathered.name.clone()));
+                        measurements.push(p_usr);
+                    }
+                    if let Some(value_sys) = diff_sys {
+                        let p_sys: MeasurementPoint = MeasurementPoint::new(
+                            timestamp,
+                            self.metrics.time_used_system_mode,
+                            Resource::LocalMachine,
+                            consumer.clone(),
+                            value_sys as u64,
+                        )
+                        .with_attr("pod", AttributeValue::String(metrics_gathered.name.clone()));
+                        measurements.push(p_sys);
+                    }
+                },
+                Err(_) => {
+                    element_unreachable.push(metric_file.name.clone());
+                }
             };
-            let diff_usr = match counter_usr.update(metrics.time_used_user_mode) {
-                CounterDiffUpdate::FirstTime => None,
-                CounterDiffUpdate::Difference(diff) => Some(diff),
-                CounterDiffUpdate::CorrectedDifference(diff) => Some(diff),
-            };
-            let diff_sys = match counter_sys.update(metrics.time_used_system_mode) {
-                CounterDiffUpdate::FirstTime => None,
-                CounterDiffUpdate::Difference(diff) => Some(diff),
-                CounterDiffUpdate::CorrectedDifference(diff) => Some(diff),
-            };
-            let consumer = ResourceConsumer::ControlGroup {
-                path: (metric_file.path.to_string_lossy().to_string().into()),
-            };
-            if let Some(value_tot) = diff_tot {
-                let p_tot: MeasurementPoint = MeasurementPoint::new(
-                    timestamp,
-                    self.metrics.time_used_tot,
-                    Resource::LocalMachine,
-                    consumer.clone(),
-                    value_tot as u64,
-                )
-                .with_attr("pod", AttributeValue::String(metrics.name.clone()));
-                measurements.push(p_tot);
+        }
+        //Clean vector of all unreachable elements
+        for to_delete in  element_unreachable.iter(){
+            if let Some(index) = self.metric_and_counter.iter().position(|(metric_file, _, _, _)| metric_file.name == *to_delete) {
+                self.metric_and_counter.swap_remove(index);
             }
-            if let Some(value_usr) = diff_usr {
-                let p_usr: MeasurementPoint = MeasurementPoint::new(
-                    timestamp,
-                    self.metrics.time_used_user_mode,
-                    Resource::LocalMachine,
-                    consumer.clone(),
-                    value_usr as u64,
-                )
-                .with_attr("pod", AttributeValue::String(metrics.name.clone()));
-                measurements.push(p_usr);
+        }
+
+        let mut buffer = [0; 1024];
+        let events = if let Ok(mut inotify_guard) = INOTIFY_VAR.lock() {
+            if let Some(inotify) = inotify_guard.as_mut() {
+                match inotify.read_events(&mut buffer) {
+                    Ok(events) => Some(events),
+                    Err(error) if error.kind() == std::io::ErrorKind::WouldBlock => None,
+                    Err(_) => panic!("Error while reading events"),
+                }
+            } else {
+                None
             }
-            if let Some(value_sys) = diff_sys {
-                let p_sys: MeasurementPoint = MeasurementPoint::new(
-                    timestamp,
-                    self.metrics.time_used_system_mode,
-                    Resource::LocalMachine,
-                    consumer.clone(),
-                    value_sys as u64,
-                )
-                .with_attr("pod", AttributeValue::String(metrics.name.clone()));
-                measurements.push(p_sys);
+        } else {
+            None
+        };
+        
+        if let Some(events) = events {
+            for event in events {
+                // Example output: Event: Event { wd: WatchDescriptor { id: 1, fd: (Weak) }, mask: CREATE | ISDIR, cookie: 0, name: Some("test.slice") }
+                if event.mask.contains(EventMask::CREATE | EventMask::ISDIR) {
+                    let raw_name: &str = event.name.unwrap().to_str().expect("Error when converting event.name to str");
+                    let name: String = raw_name.strip_suffix(".slice").unwrap_or(raw_name).to_owned();                
+                    let map_guard = MAP_FD.lock().unwrap();
+                    let tmp_path = map_guard.get(&event.wd.get_watch_descriptor_id()).unwrap();
+                    let mut full_tmp_path: PathBuf = tmp_path.clone();
+                    full_tmp_path.push(raw_name);
+                    let mut final_path: PathBuf = full_tmp_path.clone();
+                    final_path.push("cpu.stat");
+                    let file_desc = File::open(&final_path).map_err(|_| format!("failed to open file {}", final_path.display())).unwrap();
+                    let metric_file = CgroupV2MetricFile {
+                        name: name.to_owned(),
+                        path: full_tmp_path.to_path_buf(),
+                        file: file_desc,
+                    };
+                    self.add_entry(metric_file)
+                }
+                if event.mask.contains(EventMask::DELETE | EventMask::ISDIR){
+                    let raw_name: String = event.name.unwrap().to_str().expect("Error when converting event.name to str").to_owned();
+                    if let Some(index) = self.metric_and_counter.iter().position(|(metric_file, _, _, _)| metric_file.name == *raw_name) {
+                        println!("REMOVED: {:?}", raw_name);
+                        self.metric_and_counter.swap_remove(index);
+                    }
+                }
             }
         }
         Ok(())
--- plugin-k8s/src/lib.rs
+++ plugin-k8s/src/lib.rs
@@ -6,8 +6,14 @@ use alumet::{
     },
 };
 use anyhow::Context;
+use inotify::{
+    Inotify,
+    WatchMask,
+};
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
-use std::{path::PathBuf, time::Duration};
+use std::{collections::HashMap, path::PathBuf, sync::{Arc, Mutex}, time::Duration};
+
 
 mod cgroup_v2;
 mod k8s_probe;
@@ -15,6 +21,11 @@ mod parsing_cgroupv2;
 
 use crate::cgroup_v2::CgroupV2MetricFile;
 use crate::k8s_probe::K8SProbe;
+
+lazy_static! {
+    pub static ref INOTIFY_VAR: Arc<Mutex<Option<Inotify>>> = Arc::new(Mutex::new(None));
+    pub static ref MAP_FD: Mutex<HashMap<i32, PathBuf>> = Mutex::new(HashMap::new());
+}
 
 pub struct K8sPlugin {
     config: Config,
@@ -43,7 +54,11 @@ impl AlumetPlugin for K8sPlugin {
 
     fn init(config: ConfigTable) -> anyhow::Result<Box<Self>> {
         let config = deserialize_config(config).context("invalid config")?;
-        Ok(Box::new(K8sPlugin { config }))
+
+        if let Ok(mut inotify_guard) = INOTIFY_VAR.lock() {
+            *inotify_guard = Some(Inotify::init().expect("Failed to initialize an inotify instance"));
+        }    
+        Ok(Box::new(K8sPlugin { config: config }))
     }
 
     fn start(&mut self, alumet: &mut alumet::plugin::AlumetStart) -> anyhow::Result<()> {
@@ -52,14 +67,32 @@ impl AlumetPlugin for K8sPlugin {
             anyhow::bail!("Cgroups v2 are not being used!");
         }
         let final_li_metric_file: Vec<CgroupV2MetricFile> = cgroup_v2::list_all_k8s_pods_file("/sys/fs/cgroup/kubepods.slice/")?;
+        for directory_to_watch in &final_li_metric_file {
+            if let Ok(mut inotify_guard) = INOTIFY_VAR.lock() {
+                if let Some(inotify) = inotify_guard.as_mut() {
+                    let watch_descriptor = inotify.watches().add(
+                        directory_to_watch.path.clone(),
+                        WatchMask::CREATE | WatchMask::DELETE,
+                    );
+                    MAP_FD.lock().unwrap().entry(watch_descriptor.unwrap().get_watch_descriptor_id()).or_insert(directory_to_watch.path.clone());
+                }
+            }
+        }
         let metrics_result = k8s_probe::Metrics::new(alumet);
         let metrics = metrics_result?;
         let probe = K8SProbe::new(metrics.clone(), final_li_metric_file)?;
         alumet.add_source(Box::new(probe), TriggerSpec::at_interval(self.config.poll_interval));
+       
         return Ok(());
     }
 
     fn stop(&mut self) -> anyhow::Result<()> {
+        if let Ok(mut inotify_guard) = INOTIFY_VAR.lock() {
+            if let Some(_inotify) = inotify_guard.as_mut() {
+                inotify_guard.take().expect("Trying to close None Inotify").close().expect("Failed to close inotify instance");
+            }
+            *inotify_guard = None;
+        }
         Ok(())
     }
 }
