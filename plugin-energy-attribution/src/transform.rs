a4657d3287b1560c5f906254be21bae92763f10f
Titouan Jouffray <titouan.jouffray@eviden.com>
2024-06-21 14:59:11 +0200

    Add a filter on measurement (temp)

--- plugin-energy-attribution/src/transform.rs
+++ plugin-energy-attribution/src/transform.rs
@@ -1,6 +1,6 @@
-use std::sync::{Arc, Mutex};
+use std::{string, sync::{Arc, Mutex}};
 
-use alumet::{measurement::MeasurementPoint, pipeline::Transform};
+use alumet::{measurement::{AttributeValue, MeasurementPoint}, pipeline::Transform, resources::Resource};
 
 pub struct EnergyAttributionTransform {
     pub metrics: Arc<Mutex<super::Metrics>>,
@@ -14,44 +14,65 @@ impl Transform for EnergyAttributionTransform {
         &mut self,
         measurements: &mut alumet::measurement::MeasurementBuffer,
     ) -> Result<(), alumet::pipeline::TransformError> {
-        // let metrics = self.metrics.lock().unwrap();
+        let metrics = self.metrics.lock().unwrap();
 
-        // let pod_id = metrics.cpu_usage_per_pod.unwrap();
-        // let rapl_id = metrics.rapl_consumed_energy.unwrap();
+        let pod_id = metrics.cpu_usage_per_pod.unwrap();
+        let rapl_id = metrics.rapl_consumed_energy.unwrap().as_u64();
         
-        // for m in measurements.clone().iter() {
-        //     match m.metric {
-        //         rapl_id => {
-        //             self.buffer_rapl.push(m.clone());
-        //         },
-        //         pod_id => {
-        //             self.buffer_pod.push(m.clone());
-        //         },
-        //         _ => {
-        //             continue;
-        //         }
-        //     }
-        // }
-        // println!("pod_id value: {pod_id:?}");
-        // println!("rapl_id value: {rapl_id:?}");
+        let besteffort = "besteffort".to_string();
+        let burstable = "burstable".to_string();
 
-        // // on regarde temps ecoulé total CPU tc2-tc1 -> tcpu (buffer_pod.last - buffer_pod.first)
-        // // on regarde temps ecoulé total rapl tr2-tr1 -> trapl (buffer_rapl.last - buffer_rapl.first)
-        // // Pour chaque pod
-        // // pour chaque mesure du temps cpu on ajoute les temps passé par la mesure -> tottime_pod
-        // // pour chaque mesure rapl on ajoute les valeurs passées par la mesure -> totnrj
-        // // Fin Pour
-        // // Pour chaque pod 
-        // // valeur_conso = tottime_pod/tcpu * totnrj
-        // // Fin Pour
-        // // Pour chaque valeur_conso on push la valeur
+        for m in measurements.clone().iter() {
+            if m.metric.as_u64() == rapl_id {
+                match m.resource {
+                    Resource::CpuPackage{ id: _ }=> {
+                        self.buffer_rapl.push(m.clone());
+                    },
+                    _ => continue,
+                }
+            } else if m.metric.as_u64() == pod_id.as_u64() {
+                for (_, value) in m.attributes() {
+                    if value.to_string() != besteffort && value.to_string() != burstable {
+                            self.buffer_pod.push(m.clone());
+                    }
+                }
+            }
+        }
+        println!("pod_id value: {pod_id:?}");
+        println!("rapl_id value: {rapl_id:?}");
+
+
+        // clear the buffer
+        measurements.clear();
+
+
+        // fill it again
+
+        for m in self.buffer_pod.iter() {
+            measurements.push(m.clone());
+        }
+        
+        for m in self.buffer_rapl.iter() {
+            measurements.push(m.clone());
+        }
+
+        self.buffer_pod.clear();
+        self.buffer_rapl.clear();
+
+        // on regarde temps ecoulé total CPU tc2-tc1 -> tcpu (buffer_pod.last - buffer_pod.first)
+        // on regarde temps ecoulé total rapl tr2-tr1 -> trapl (buffer_rapl.last - buffer_rapl.first)
+        // Pour chaque pod
+        // pour chaque mesure du temps cpu on ajoute les temps passé par la mesure -> tottime_pod
+        // pour chaque mesure rapl on ajoute les valeurs passées par la mesure -> totnrj
+        // Fin Pour
+        // Pour chaque pod 
+        // valeur_conso = tottime_pod/tcpu * totnrj
+        // Fin Pour
+        // Pour chaque valeur_conso on push la valeur
         
         
         // todo!("attribution")
-        println!("Measurement length: {:?}", measurements.len());
-        for m in measurements.clone().iter(){
-            println!("{m:?}");
-        }
+
         Ok(())
     }
 }
