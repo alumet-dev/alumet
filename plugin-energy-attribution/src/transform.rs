c9ef5ac527ac3f0666eed31d1d80b219b4b40d3c
Titouan Jouffray <titouan.jouffray@eviden.com>
2024-06-25 09:20:52 +0200

    fix(plugin-energy-attribution): Fix the compute when data is missing
    The attribution was computed when a cpu rapl metric was received. We are now waiting for both buffers to be filled with at least two columns of timestamp before doing the computation.

--- plugin-energy-attribution/src/transform.rs
+++ plugin-energy-attribution/src/transform.rs
@@ -37,59 +37,8 @@ impl Transform for EnergyAttributionTransform {
                     // If the metric is rapl then we insert only the cpu package one in the buffer
                     Resource::CpuPackage{ id: _ }=> {
                         let id = m.timestamp.get_sec();
-
-                        // self.buffer_rapl.insert(id, m.clone()).unwrap();
-
-                        if !self.buffer_pod.contains_key(&id) {
-                            continue;
-                        }
-
-                        // let length = self.buffer_pod.get(&id).unwrap().len() as f64;
-                        let utime_sum = self.buffer_pod
-                            .get(&id).unwrap()
-                            .iter().map(|x| x.clone().value)
-                            .reduce(|x, y| {
-                                match (x, y) {
-                                    (WrappedMeasurementValue::F64(fx), WrappedMeasurementValue::F64(fy)) => {
-                                        WrappedMeasurementValue::F64(fx + fy)
-                                    }
-                                    (WrappedMeasurementValue::U64(ux), WrappedMeasurementValue::U64(uy)) => {
-                                        WrappedMeasurementValue::U64(ux + uy)
-                                    }
-                                    (_, _) => panic!("Pas normal"), // TODO Fix this panic line
-                                }
-                        }).unwrap();
-
-                        let utime_sum_f64 = match utime_sum {
-                            WrappedMeasurementValue::F64(fx) => fx,
-                            WrappedMeasurementValue::U64(ux) => ux as f64,
-                        };
-
-                        let mut new_m: MeasurementPoint;
-
-                        for point in self.buffer_pod.remove(&id).unwrap().iter() {
-                            let cur_utime_f64 = match point.value {
-                                WrappedMeasurementValue::F64(fx) => fx,
-                                WrappedMeasurementValue::U64(ux) => ux as f64,
-                            };
-                            
-                            new_m = MeasurementPoint::new(
-                                m.timestamp, 
-                                metric_id,
-                                point.resource.clone(),
-                                point.consumer.clone(),
-                                match m.value {
-                                    WrappedMeasurementValue::F64(fx) => cur_utime_f64 / utime_sum_f64 * fx,
-                                    WrappedMeasurementValue::U64(ux) => cur_utime_f64 / utime_sum_f64 * (ux as f64),
-                                }
-                            );
-
-                            for (key, value) in point.clone().attributes() {
-                                new_m = new_m.with_attr(key.to_owned(), value.clone());
-                            }
-
-                            measurements.push(new_m.clone());
-                        }
+                        
+                        self.buffer_rapl.insert(id, m.clone());
                     },
                     _ => continue,
                 }
@@ -109,6 +58,63 @@ impl Transform for EnergyAttributionTransform {
                         }
                     }
                 }
+            }
+        }
+
+        // If the buffers do have enough (every) MeasurementPoints
+        // Then we compute the energy attribution
+        while self.buffer_rapl.len() >= 2 && self.buffer_pod.len() >= 2 {
+            let rapl_mini_id = self.buffer_rapl.keys().reduce(|x, y| if x < y { x } else { y }).unwrap().clone();
+            if !self.buffer_pod.contains_key(&rapl_mini_id) {
+                panic!("pas normal");
+            }
+
+            let m = self.buffer_rapl.remove(&rapl_mini_id).unwrap();
+
+            let utime_sum = self.buffer_pod
+                .get(&rapl_mini_id).unwrap()
+                .iter().map(|x| x.clone().value)
+                .reduce(|x, y| {
+                    match (x, y) {
+                        (WrappedMeasurementValue::F64(fx), WrappedMeasurementValue::F64(fy)) => {
+                            WrappedMeasurementValue::F64(fx + fy)
+                        }
+                        (WrappedMeasurementValue::U64(ux), WrappedMeasurementValue::U64(uy)) => {
+                            WrappedMeasurementValue::U64(ux + uy)
+                        }
+                        (_, _) => panic!("Pas normal"), // TODO Fix this panic line
+                    }
+            }).unwrap();
+
+            let utime_sum_f64 = match utime_sum {
+                WrappedMeasurementValue::F64(fx) => fx,
+                WrappedMeasurementValue::U64(ux) => ux as f64,
+            };
+
+            let mut new_m: MeasurementPoint;
+
+            for point in self.buffer_pod.remove(&rapl_mini_id).unwrap().iter() {
+                let cur_utime_f64 = match point.value {
+                    WrappedMeasurementValue::F64(fx) => fx,
+                    WrappedMeasurementValue::U64(ux) => ux as f64,
+                };
+                
+                new_m = MeasurementPoint::new(
+                    m.timestamp, 
+                    metric_id,
+                    point.resource.clone(),
+                    point.consumer.clone(),
+                    match m.value {
+                        WrappedMeasurementValue::F64(fx) => cur_utime_f64 / utime_sum_f64 * fx,
+                        WrappedMeasurementValue::U64(ux) => cur_utime_f64 / utime_sum_f64 * (ux as f64),
+                    }
+                );
+
+                for (key, value) in point.clone().attributes() {
+                    new_m = new_m.with_attr(key.to_owned(), value.clone());
+                }
+
+                measurements.push(new_m.clone());
             }
         }
 
