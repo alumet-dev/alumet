0a7275ec4e813487f6394df4f05c14ad7831bf3c
Titouan Jouffray <titouan.jouffray@eviden.com>
2024-06-26 08:42:40 +0200

    feat(alumet): Add get_sec() to Timestamp

--- alumet/src/measurement.rs
+++ alumet/src/measurement.rs
@@ -27,6 +27,7 @@
 
 use core::fmt;
 use std::borrow::Cow;
+use std::time::UNIX_EPOCH;
 use fxhash::FxBuildHasher;
 use smallvec::SmallVec;
 use std::{collections::HashMap, fmt::Display, time::SystemTime};
@@ -40,7 +41,7 @@ use super::resources::Resource;
 ///
 /// Measurement points may also have attributes.
 /// Only certain types of values and attributes are allowed, see [`MeasurementType`] and [`AttributeValue`].
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct MeasurementPoint {
     /// The metric that has been measured.
     pub metric: RawMetricId,
@@ -164,6 +165,10 @@ impl Timestamp {
     /// Returns a `Timestamp` representing the current system time.
     pub fn now() -> Self {
         Self(SystemTime::now())
+    }
+
+    pub fn get_sec(&self) -> u64 {
+        self.0.duration_since(UNIX_EPOCH).unwrap().as_secs()
     }
 }
 
