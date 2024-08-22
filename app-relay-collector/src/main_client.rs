d1acc7255f9e7d9ef1809dbcb7c7cabf350b25b2
Guillaume Raffin <theelectronwill@gmail.com>
2024-08-22 12:20:40 +0200

    agents: Add a warning when running in debug mode

--- app-relay-collector/src/main_client.rs
+++ app-relay-collector/src/main_client.rs
@@ -12,6 +12,11 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     log::info!("Starting ALUMET relay agent v{VERSION}");
+    // Print a warning if we are running in debug mode.
+    #[cfg(debug_assertions)]
+    {
+        log::warn!("DEBUG assertions are enabled, this build of Alumet is fine for debugging, but not for production.");
+    }
 
     // Parse command-line arguments.
     let args = Args::parse();
