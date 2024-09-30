d3e259db732d87632bdeb2855e48c0f6a717f0df
Guillaume Raffin <theelectronwill@gmail.com>
2024-09-30 13:14:33 +0200

    feat(plugin-procfs): Allow to monitor individual processes on demand by reacting to Alumet events

--- plugin-procfs/src/lib.rs
+++ plugin-procfs/src/lib.rs
@@ -1,14 +1,14 @@
-use std::time::Duration;
-
 use alumet::{
     pipeline::trigger::TriggerSpec,
-    plugin::rust::{deserialize_config, serialize_config, AlumetPlugin},
+    plugin::{
+        event,
+        rust::{deserialize_config, serialize_config, AlumetPlugin},
+    },
+    resources::ResourceConsumer,
     units::{PrefixedUnit, Unit},
 };
 use anyhow::Context;
 use procfs::{Current, CurrentSI};
-use regex::Regex;
-use serde::{Deserialize, Serialize};
 
 mod kernel;
 mod memory;
@@ -16,7 +16,7 @@ mod process;
 mod serde_regex;
 
 pub struct ProcfsPlugin {
-    config: Option<Config>,
+    config: Option<config::Config>,
 }
 
 impl AlumetPlugin for ProcfsPlugin {
@@ -29,42 +29,25 @@ impl AlumetPlugin for ProcfsPlugin {
     }
 
     fn default_config() -> anyhow::Result<Option<alumet::plugin::ConfigTable>> {
-        Ok(Some(serialize_config(Config::default())?))
+        Ok(Some(serialize_config(config::Config::default())?))
     }
 
     fn init(config: alumet::plugin::ConfigTable) -> anyhow::Result<Box<Self>> {
-        let config: Config = deserialize_config(config)?;
+        let config: config::Config = deserialize_config(config)?;
         Ok(Box::new(ProcfsPlugin { config: Some(config) }))
     }
 
     fn start(&mut self, alumet: &mut alumet::plugin::AlumetPluginStart) -> anyhow::Result<()> {
         // TODO allow to add a source in a "paused" state for later activation?
-        // Start kernel and meminfo sources, if enabled
+
+        // Start the procfs-related sources that are enabled, according to the config.
+        // Each subconfig is moved into the corresponding function, hence `config` is partially moved.
         let config = self.config.take().unwrap();
         if config.kernel.enabled {
-            let trigger = TriggerSpec::at_interval(config.kernel.poll_interval);
-            let metrics = kernel::KernelMetrics::new(alumet).context("unable to register metrics for kernel probe")?;
-            let source = kernel::KernelStatsProbe::new(metrics, procfs::KernelStats::PATH)
-                .context("unable to create kernel probe")?;
-            alumet.add_source(Box::new(source), trigger);
+            start_kernel_probe(config.kernel, alumet)?;
         }
         if config.memory.enabled {
-            let trigger = TriggerSpec::at_interval(config.memory.poll_interval);
-            let metrics: anyhow::Result<Vec<_>> = config
-                .memory
-                .metrics
-                .into_iter()
-                .map(|procfs_entry_name| {
-                    let metric_name = convert_to_snake_case(&procfs_entry_name);
-                    let metric = alumet
-                        .create_metric(&metric_name, PrefixedUnit::kilo(Unit::Byte), "?")
-                        .with_context(|| format!("unable to register metric {metric_name} for memory probe"))?;
-                    Ok((procfs_entry_name, metric))
-                })
-                .collect();
-            let source =
-                memory::MeminfoProbe::new(metrics?, procfs::Meminfo::PATH).context("unable to create memory probe")?;
-            alumet.add_source(Box::new(source), trigger);
+            start_memory_probe(config.memory, alumet)?;
         }
         if config.processes.enabled {
             let metrics = process::ProcessMetrics {
@@ -75,33 +58,15 @@ impl AlumetPlugin for ProcfsPlugin {
                     .create_metric("process_memory", PrefixedUnit::kilo(Unit::Byte), "Memory usage")
                     .context("unable to register metric memory for process probe")?,
             };
-            let trigger = TriggerSpec::at_interval(config.processes.refresh_interval);
-            let groups = config
-                .processes
-                .groups
-                .into_iter()
-                .map(|group| {
-                    let filter = process::ProcessFilter {
-                        pid: group.pid.map(|x| i32::try_from(x).unwrap_or(-1)),
-                        ppid: group.ppid.map(|x| i32::try_from(x).unwrap_or(-1)),
-                        exe_regex: group.exe_regex,
-                    };
-                    let settings = process::MonitoringSettings {
-                        poll_interval: group.poll_interval,
-                        flush_interval: group.flush_interval,
-                    };
-                    (filter, settings)
-                })
-                .collect();
-            // We need a ScopedControlHandle to spawn new sources in the ProcessWatcher,
-            // therefore it needs to be created after the pipeline startup.
-            alumet.on_pipeline_start(move |ctx| {
-                let control_handle = ctx.pipeline_control();
-                let source = Box::new(process::ProcessWatcher::new(control_handle.clone(), metrics, groups));
-                control_handle
-                    .add_source("process-watcher", source, trigger)
-                    .context("failed to add the process-watcher to the pipeline")
-            });
+
+            match config.processes.strategy {
+                config::ProcessWatchStrategy::SystemWatcher => {
+                    start_process_watcher(config.processes, alumet, metrics);
+                }
+                config::ProcessWatchStrategy::InternalEvent => {
+                    setup_process_event_listener(config.processes, alumet, metrics);
+                }
+            }
         }
         Ok(())
     }
@@ -111,120 +76,269 @@ impl AlumetPlugin for ProcfsPlugin {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-struct Config {
-    kernel: KernelStatsMonitoring,
-    memory: MeminfoMonitoring,
-    processes: ProcessMonitoring,
+fn start_kernel_probe(
+    config_kernel: config::KernelStatsMonitoring,
+    alumet: &mut alumet::plugin::AlumetPluginStart<'_>,
+) -> Result<(), anyhow::Error> {
+    let trigger = TriggerSpec::at_interval(config_kernel.poll_interval);
+    let metrics = kernel::KernelMetrics::new(alumet).context("unable to register metrics for kernel probe")?;
+    let source =
+        kernel::KernelStatsProbe::new(metrics, procfs::KernelStats::PATH).context("unable to create kernel probe")?;
+    alumet.add_source(Box::new(source), trigger);
+    Ok(())
 }
 
-#[derive(Serialize, Deserialize)]
-struct KernelStatsMonitoring {
-    #[serde(default = "default_enabled")]
-    enabled: bool,
-    #[serde(with = "humantime_serde")]
-    poll_interval: Duration,
+fn start_memory_probe(
+    config_memory: config::MeminfoMonitoring,
+    alumet: &mut alumet::plugin::AlumetPluginStart<'_>,
+) -> Result<(), anyhow::Error> {
+    let trigger = TriggerSpec::at_interval(config_memory.poll_interval);
+    let metrics: anyhow::Result<Vec<_>> = config_memory
+        .metrics
+        .into_iter()
+        .map(|procfs_entry_name| {
+            let metric_name = convert_to_snake_case(&procfs_entry_name);
+            let metric = alumet
+                .create_metric(&metric_name, PrefixedUnit::kilo(Unit::Byte), "?")
+                .with_context(|| format!("unable to register metric {metric_name} for memory probe"))?;
+            Ok((procfs_entry_name, metric))
+        })
+        .collect();
+    let source = memory::MeminfoProbe::new(metrics?, procfs::Meminfo::PATH).context("unable to create memory probe")?;
+    alumet.add_source(Box::new(source), trigger);
+    Ok(())
 }
 
-#[derive(Serialize, Deserialize)]
-struct MeminfoMonitoring {
-    #[serde(default = "default_enabled")]
-    enabled: bool,
-    #[serde(with = "humantime_serde")]
-    poll_interval: Duration,
-    /// The entry to parse from /proc/meminfo.
-    metrics: Vec<String>,
+fn start_process_watcher(
+    config_processes: config::ProcessMonitoring,
+    alumet: &mut alumet::plugin::AlumetPluginStart<'_>,
+    metrics: process::ProcessMetrics,
+) {
+    let trigger = TriggerSpec::at_interval(config_processes.refresh_interval);
+    let groups = config_processes
+        .groups
+        .into_iter()
+        .map(|group| {
+            let filter = process::ProcessFilter {
+                pid: group.pid.map(|x| i32::try_from(x).unwrap_or(-1)),
+                ppid: group.ppid.map(|x| i32::try_from(x).unwrap_or(-1)),
+                exe_regex: group.exe_regex,
+            };
+            let settings = process::MonitoringSettings {
+                poll_interval: group.poll_interval,
+                flush_interval: group.flush_interval,
+            };
+            (filter, settings)
+        })
+        .collect();
+
+    // We need a ScopedControlHandle to spawn new sources in the ProcessWatcher,
+    // therefore it needs to be created after the pipeline startup.
+    //
+    // Note: using `on_pipeline_start` is easier than storing a state in the plugin and
+    // overriding `post_pipeline_start`.
+
+    alumet.on_pipeline_start(move |ctx| {
+        log::info!("Starting system-wide process watcher.");
+        let control_handle = ctx.pipeline_control();
+        let source = Box::new(process::ProcessWatcher::new(control_handle.clone(), metrics, groups));
+        control_handle
+            .add_source("process-watcher", source, trigger)
+            .context("failed to add the process-watcher to the pipeline")
+    });
 }
 
-#[derive(Serialize, Deserialize)]
-struct ProcessMonitoring {
-    #[serde(default = "default_enabled")]
-    enabled: bool,
-    #[serde(with = "humantime_serde")]
-    refresh_interval: Duration,
-    groups: Vec<ProcessMonitoringGroup>,
+fn setup_process_event_listener(
+    config_processes: config::ProcessMonitoring,
+    alumet: &mut alumet::plugin::AlumetPluginStart<'_>,
+    metrics: process::ProcessMetrics,
+) {
+    let settings = process::MonitoringSettings {
+        poll_interval: config_processes.events.poll_interval,
+        flush_interval: config_processes.events.flush_interval,
+    };
+
+    alumet.on_pipeline_start(move |ctx| {
+        log::info!("Setting up on-demand process watcher.");
+        let control_handle = ctx.pipeline_control();
+        let monitor = process::ManualProcessMonitor::new(control_handle.clone(), metrics, settings);
+        event::start_consumer_measurement().subscribe(move |evt| {
+            let mut control_handle = control_handle.clone();
+            let mut buf = control_handle.source_buffer_with_capacity(evt.0.len());
+            for process in evt.0 {
+                match process {
+                    ResourceConsumer::Process { pid } => {
+                        // TODO continue even in case of error (useful when there are multiple processes to watch)
+                        log::debug!("Starting to monitor process with pid {pid}");
+                        monitor.start_monitoring(pid.try_into().unwrap(), &mut buf)?;
+                    }
+                    _ => (),
+                }
+            }
+            buf.flush()?;
+            Ok(())
+        });
+        Ok(())
+    });
 }
 
-#[derive(Serialize, Deserialize)]
-struct ProcessMonitoringGroup {
-    /// Only monitor the process that has this pid.
-    pid: Option<u32>,
+mod config {
+    use std::time::Duration;
 
-    /// Only monitor the processes that have this parent pid.
-    ppid: Option<u32>,
+    use crate::serde_regex;
+    use regex::Regex;
+    use serde::{Deserialize, Serialize};
 
-    /// Only monitor the processes whose executable path matches this regex.
-    #[serde(with = "serde_regex::option")]
-    exe_regex: Option<Regex>,
+    #[derive(Serialize, Deserialize)]
+    pub struct Config {
+        pub kernel: KernelStatsMonitoring,
+        pub memory: MeminfoMonitoring,
+        pub processes: ProcessMonitoring,
+    }
 
-    /// How frequently should the processes information be refreshed.
-    #[serde(with = "humantime_serde")]
-    poll_interval: Duration,
+    #[derive(Serialize, Deserialize)]
+    pub struct KernelStatsMonitoring {
+        #[serde(default = "default_enabled")]
+        pub enabled: bool,
+        #[serde(with = "humantime_serde")]
+        pub poll_interval: Duration,
+    }
 
-    /// How frequently should the processes information be flushed to the rest of the pipeline.
-    #[serde(with = "humantime_serde")]
-    flush_interval: Duration,
-}
+    #[derive(Serialize, Deserialize)]
+    pub struct MeminfoMonitoring {
+        #[serde(default = "default_enabled")]
+        pub enabled: bool,
+        #[serde(with = "humantime_serde")]
+        pub poll_interval: Duration,
+        /// The entry to parse from /proc/meminfo.
+        pub metrics: Vec<String>,
+    }
 
-impl Default for KernelStatsMonitoring {
-    fn default() -> Self {
-        Self {
-            enabled: true,
-            poll_interval: Duration::from_secs(5),
+    #[derive(Serialize, Deserialize)]
+    pub struct ProcessMonitoring {
+        /// `true` to enable the monitoring of processes.
+        #[serde(default = "default_enabled")]
+        pub enabled: bool,
+
+        /// Watcher refresh interval.
+        #[serde(with = "humantime_serde")]
+        pub refresh_interval: Duration,
+
+        /// Groups of processes to monitor when detected.
+        pub groups: Vec<ProcessMonitoringGroup>,
+
+        /// `true` to watch for new processes, `false` to only react to Alumet events.
+        #[serde(default = "default_watch_strategy")]
+        pub strategy: ProcessWatchStrategy,
+        pub events: EventModeProcessMonitoring,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub enum ProcessWatchStrategy {
+        #[serde(rename = "watcher")]
+        SystemWatcher,
+        #[serde(rename = "event")]
+        InternalEvent,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct EventModeProcessMonitoring {
+        #[serde(with = "humantime_serde")]
+        pub poll_interval: Duration,
+        #[serde(with = "humantime_serde")]
+        pub flush_interval: Duration,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct ProcessMonitoringGroup {
+        /// Only monitor the process that has this pid.
+        pub pid: Option<u32>,
+
+        /// Only monitor the processes that have this parent pid.
+        pub ppid: Option<u32>,
+
+        /// Only monitor the processes whose executable path matches this regex.
+        #[serde(with = "serde_regex::option")]
+        pub exe_regex: Option<Regex>,
+
+        /// How frequently should the processes information be refreshed.
+        #[serde(with = "humantime_serde")]
+        pub poll_interval: Duration,
+
+        /// How frequently should the processes information be flushed to the rest of the pipeline.
+        #[serde(with = "humantime_serde")]
+        pub flush_interval: Duration,
+    }
+
+    impl Default for KernelStatsMonitoring {
+        fn default() -> Self {
+            Self {
+                enabled: true,
+                poll_interval: Duration::from_secs(5),
+            }
         }
     }
-}
 
-impl Default for MeminfoMonitoring {
-    fn default() -> Self {
-        Self {
-            enabled: true,
-            poll_interval: Duration::from_secs(5),
-            metrics: vec![
-                "MemTotal",
-                "MemFree",
-                "MemAvailable",
-                "Cached",
-                "SwapCached",
-                "Active",
-                "Inactive",
-                "Mapped",
-            ]
-            .into_iter()
-            .map(str::to_owned)
-            .collect(),
+    impl Default for MeminfoMonitoring {
+        fn default() -> Self {
+            Self {
+                enabled: true,
+                poll_interval: Duration::from_secs(5),
+                metrics: vec![
+                    "MemTotal",
+                    "MemFree",
+                    "MemAvailable",
+                    "Cached",
+                    "SwapCached",
+                    "Active",
+                    "Inactive",
+                    "Mapped",
+                ]
+                .into_iter()
+                .map(str::to_owned)
+                .collect(),
+            }
         }
     }
-}
 
-impl Default for ProcessMonitoring {
-    fn default() -> Self {
-        Self {
-            enabled: true,
-            refresh_interval: Duration::from_secs(2),
-            groups: vec![ProcessMonitoringGroup {
-                pid: None,
-                ppid: None,
-                exe_regex: None, // any process; Regex::new(".*").unwrap() would also work
-                poll_interval: Duration::from_secs(2),
-                flush_interval: Duration::from_secs(4),
-            }],
+    impl Default for ProcessMonitoring {
+        fn default() -> Self {
+            Self {
+                enabled: true,
+                strategy: default_watch_strategy(),
+                refresh_interval: Duration::from_secs(2),
+                groups: vec![ProcessMonitoringGroup {
+                    pid: None,
+                    ppid: None,
+                    exe_regex: None, // any process; Regex::new(".*").unwrap() would also work
+                    poll_interval: Duration::from_secs(2),
+                    flush_interval: Duration::from_secs(4),
+                }],
+                events: EventModeProcessMonitoring {
+                    poll_interval: Duration::from_secs(1),
+                    flush_interval: Duration::from_secs(4),
+                },
+            }
         }
     }
-}
 
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            kernel: KernelStatsMonitoring::default(),
-            memory: MeminfoMonitoring::default(),
-            processes: ProcessMonitoring::default(),
+    impl Default for Config {
+        fn default() -> Self {
+            Self {
+                kernel: KernelStatsMonitoring::default(),
+                memory: MeminfoMonitoring::default(),
+                processes: ProcessMonitoring::default(),
+            }
         }
     }
-}
 
-fn default_enabled() -> bool {
-    true
+    fn default_enabled() -> bool {
+        true
+    }
+
+    fn default_watch_strategy() -> ProcessWatchStrategy {
+        ProcessWatchStrategy::SystemWatcher
+    }
 }
 
 #[derive(Clone, Copy)]
