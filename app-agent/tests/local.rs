0d8d82ff0f5d0beab226df2c71a6ea35f78cb6ac
Guillaume Raffin <theelectronwill@gmail.com>
2024-10-14 13:08:22 +0200

    agents: Test the config argument

--- app-agent/tests/local.rs
+++ app-agent/tests/local.rs
@@ -10,6 +10,45 @@ fn help() {
 }
 
 #[test]
+fn args_bad_config() -> anyhow::Result<()> {
+    let tmp_dir = std::env::temp_dir().join(format!("{}-args_bad_config", env!("CARGO_CRATE_NAME")));
+    let bad_conf = tmp_dir.join("zzzzz.toml");
+    let _ = std::fs::remove_dir_all(&tmp_dir);
+
+    let bad_conf_filename = bad_conf.to_str().unwrap();
+    let output = cargo_run_tee("alumet-local-agent", &["local_x86"], &["--config", bad_conf_filename])?;
+    assert!(
+        !output.status.success(),
+        "should fail because the config directory does not exist"
+    );
+    let stdout = String::from_utf8(output.stdout)?;
+    let stderr = String::from_utf8(output.stderr)?;
+    assert!(stdout.contains(bad_conf_filename) || stderr.contains(bad_conf_filename));
+    Ok(())
+}
+
+#[test]
+fn args_regen_config() -> anyhow::Result<()> {
+    let tmp_dir = std::env::temp_dir().join(format!("{}-args_regen_config", env!("CARGO_CRATE_NAME")));
+    let conf = tmp_dir.join("config.toml");
+    let _ = std::fs::remove_file(&conf);
+    std::fs::create_dir(&tmp_dir)?;
+
+    let conf_path_str = conf.to_str().unwrap();
+    let output = cargo_run_tee(
+        "alumet-local-agent",
+        &["local_x86"],
+        &["--config", conf_path_str, "regen-config"],
+    )?;
+    assert!(output.status.success(), "command should succeed");
+
+    let conf_content =
+        std::fs::read_to_string(&conf).with_context(|| format!("config should be generated to {conf:?}"))?;
+    assert!(!conf_content.is_empty(), "config should not be empty");
+    Ok(())
+}
+
+#[test]
 fn args_output_exec() -> anyhow::Result<()> {
     let tmp_dir = std::env::temp_dir().join(format!("{}-args_output_exec", env!("CARGO_CRATE_NAME")));
     let tmp_file_out = tmp_dir.join("agent-output.csv");
