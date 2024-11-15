74853a864c14fbd21efed7e3575bb88a87725cd8
ARTHUR CHENU (ext) <arthur.chenu.external@eviden.com>
2024-11-15 14:56:53 +0100

    feat(plugin-cgroupv2): add memory metrics and unit tests

--- plugin-cgroupv2/src/k8s/probe.rs
+++ plugin-cgroupv2/src/k8s/probe.rs
@@ -1,24 +1,28 @@
 use alumet::{
     measurement::{AttributeValue, MeasurementAccumulator, MeasurementPoint, Timestamp},
     metrics::TypedMetricId,
-    pipeline::elements::error::PollError,
+    pipeline::elements::error::{PollError, PollRetry},
     plugin::util::CounterDiff,
     resources::{Resource, ResourceConsumer},
 };
-use anyhow::Result;
-
-use crate::cgroupv2::{CgroupV2Metric, Metrics};
+use anyhow::{Context, Result};
 
 use super::utils::{gather_value, CgroupV2MetricFile};
+use crate::cgroupv2::{CgroupMeasurements, Metrics};
 
 pub struct K8SProbe {
     pub cgroup_v2_metric_file: CgroupV2MetricFile,
     pub time_tot: CounterDiff,
     pub time_usr: CounterDiff,
     pub time_sys: CounterDiff,
-    pub time_used_tot: TypedMetricId<u64>,
-    pub time_used_user_mode: TypedMetricId<u64>,
-    pub time_used_system_mode: TypedMetricId<u64>,
+    pub cpu_time_tot: TypedMetricId<u64>,
+    pub cpu_time_user_mode: TypedMetricId<u64>,
+    pub cpu_time_system_mode: TypedMetricId<u64>,
+    pub memory_anon: TypedMetricId<u64>,
+    pub memory_file: TypedMetricId<u64>,
+    pub memory_kernel: TypedMetricId<u64>,
+    pub memory_pagetables: TypedMetricId<u64>,
+    pub memory_total: TypedMetricId<u64>,
 }
 
 impl K8SProbe {
@@ -34,66 +38,142 @@ impl K8SProbe {
             time_tot: counter_tot,
             time_usr: counter_usr,
             time_sys: counter_sys,
-            time_used_tot: metric.time_used_tot,
-            time_used_system_mode: metric.time_used_system_mode,
-            time_used_user_mode: metric.time_used_user_mode,
+            cpu_time_tot: metric.cpu_time_total,
+            cpu_time_user_mode: metric.cpu_time_user_mode,
+            cpu_time_system_mode: metric.cpu_time_system_mode,
+            memory_anon: metric.memory_anonymous,
+            memory_file: metric.memory_file,
+            memory_kernel: metric.memory_kernel,
+            memory_pagetables: metric.memory_pagetables,
+            memory_total: metric.memory_total,
         })
     }
 }
 
 impl alumet::pipeline::Source for K8SProbe {
     fn poll(&mut self, measurements: &mut MeasurementAccumulator, timestamp: Timestamp) -> Result<(), PollError> {
-        let mut file_buffer = String::new();
-        let metrics: CgroupV2Metric = gather_value(&mut self.cgroup_v2_metric_file, &mut file_buffer)?;
-        let diff_tot = self.time_tot.update(metrics.time_used_tot).difference();
-        let diff_usr = self.time_usr.update(metrics.time_used_user_mode).difference();
-        let diff_sys = self.time_sys.update(metrics.time_used_system_mode).difference();
-        let consumer = ResourceConsumer::ControlGroup {
-            path: (self.cgroup_v2_metric_file.path.to_string_lossy().to_string().into()),
-        };
-        if let Some(value_tot) = diff_tot {
-            let p_tot: MeasurementPoint = MeasurementPoint::new(
+        /// Create a measurement point with given value,
+        /// the `LocalMachine` resource and some attributes related to the pod.
+        fn create_measurement_point(
+            timestamp: Timestamp,
+            metric_id: TypedMetricId<u64>,
+            resource_consumer: ResourceConsumer,
+            value_measured: u64,
+            metrics_param: &CgroupMeasurements,
+        ) -> MeasurementPoint {
+            MeasurementPoint::new(
                 timestamp,
-                self.time_used_tot,
+                metric_id,
                 Resource::LocalMachine,
-                consumer.clone(),
-                value_tot,
+                resource_consumer,
+                value_measured,
             )
-            .with_attr("uid", AttributeValue::String(metrics.uid.clone()))
-            .with_attr("name", AttributeValue::String(metrics.name.clone()))
-            .with_attr("namespace", AttributeValue::String(metrics.namespace.clone()))
-            .with_attr("node", AttributeValue::String(metrics.node.clone()));
+            .with_attr("uid", AttributeValue::String(metrics_param.pod_uid.clone()))
+            .with_attr("name", AttributeValue::String(metrics_param.pod_name.clone()))
+            .with_attr("namespace", AttributeValue::String(metrics_param.namespace.clone()))
+            .with_attr("node", AttributeValue::String(metrics_param.node.clone()))
+        }
+
+        let mut buffer = String::new();
+        let metrics = gather_value(&mut self.cgroup_v2_metric_file, &mut buffer)
+            .context("Error get value")
+            .retry_poll()?;
+
+        let diff_tot = self.time_tot.update(metrics.cpu_time_total).difference();
+        let diff_usr = self.time_usr.update(metrics.cpu_time_user_mode).difference();
+        let diff_sys = self.time_sys.update(metrics.cpu_time_system_mode).difference();
+
+        // Push cpu total usage measure for user and system
+        if let Some(value_tot) = diff_tot {
+            let p_tot = create_measurement_point(
+                timestamp,
+                self.cpu_time_tot,
+                self.cgroup_v2_metric_file.consumer_cpu.clone(),
+                value_tot,
+                &metrics,
+            );
             measurements.push(p_tot);
         }
+
+        // Push cpu usage measure for user
         if let Some(value_usr) = diff_usr {
-            let p_usr: MeasurementPoint = MeasurementPoint::new(
+            let p_usr = create_measurement_point(
                 timestamp,
-                self.time_used_user_mode,
-                Resource::LocalMachine,
-                consumer.clone(),
+                self.cpu_time_user_mode,
+                self.cgroup_v2_metric_file.consumer_cpu.clone(),
                 value_usr,
-            )
-            .with_attr("uid", AttributeValue::String(metrics.uid.clone()))
-            .with_attr("name", AttributeValue::String(metrics.name.clone()))
-            .with_attr("namespace", AttributeValue::String(metrics.namespace.clone()))
-            .with_attr("node", AttributeValue::String(metrics.node.clone()));
+                &metrics,
+            );
             measurements.push(p_usr);
         }
-        if let Some(value_sys) = diff_sys {
-            let p_sys: MeasurementPoint = MeasurementPoint::new(
-                timestamp,
-                self.time_used_system_mode,
-                Resource::LocalMachine,
-                consumer.clone(),
-                value_sys,
-            )
-            .with_attr("uid", AttributeValue::String(metrics.uid.clone()))
-            .with_attr("name", AttributeValue::String(metrics.name.clone()))
-            .with_attr("namespace", AttributeValue::String(metrics.namespace.clone()))
-            .with_attr("node", AttributeValue::String(metrics.node.clone()));
 
+        // Push cpu usage measure for system
+        if let Some(value_sys) = diff_sys {
+            let p_sys = create_measurement_point(
+                timestamp,
+                self.cpu_time_system_mode,
+                self.cgroup_v2_metric_file.consumer_cpu.clone(),
+                value_sys,
+                &metrics,
+            );
             measurements.push(p_sys);
         }
+
+        // Push anonymous used memory measure corresponding to running process and various allocated memory
+        let mem_anon_value = metrics.memory_anonymous;
+        let m_anon = create_measurement_point(
+            timestamp,
+            self.memory_anon,
+            self.cgroup_v2_metric_file.consumer_memory.clone(),
+            mem_anon_value,
+            &metrics,
+        );
+        measurements.push(m_anon);
+
+        // Push files memory measure, corresponding to open files and descriptors
+        let mem_file_value = metrics.memory_file;
+        let m_file = create_measurement_point(
+            timestamp,
+            self.memory_file,
+            self.cgroup_v2_metric_file.consumer_memory.clone(),
+            mem_file_value,
+            &metrics,
+        );
+        measurements.push(m_file);
+
+        // Push kernel memory measure
+        let mem_kernel_value = metrics.memory_kernel;
+        let m_ker = create_measurement_point(
+            timestamp,
+            self.memory_kernel,
+            self.cgroup_v2_metric_file.consumer_memory.clone(),
+            mem_kernel_value,
+            &metrics,
+        );
+        measurements.push(m_ker);
+
+        // Push pagetables memory measure
+        let mem_pagetables_value = metrics.memory_pagetables;
+        let m_pgt = create_measurement_point(
+            timestamp,
+            self.memory_pagetables,
+            self.cgroup_v2_metric_file.consumer_memory.clone(),
+            mem_pagetables_value,
+            &metrics,
+        );
+        measurements.push(m_pgt);
+
+        // Push total memory used by cgroup measure
+        let mem_total_value = mem_anon_value + mem_file_value + mem_kernel_value + mem_pagetables_value;
+        let m_tot = create_measurement_point(
+            timestamp,
+            self.memory_total,
+            self.cgroup_v2_metric_file.consumer_memory.clone(),
+            mem_total_value,
+            &metrics,
+        );
+        measurements.push(m_tot);
+
         Ok(())
     }
 }
