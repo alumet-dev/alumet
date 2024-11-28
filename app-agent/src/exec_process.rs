8f345f51733349b40de0389f52d5d3a73f8bc36e
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@eviden.com>
2024-11-28 15:07:38 +0100

    feat(exec_process): Added a better error handling

--- app-agent/src/exec_process.rs
+++ app-agent/src/exec_process.rs
@@ -1,6 +1,6 @@
 //! Tie Alumet to a running process.
 
-use std::{fs, os::unix::fs::PermissionsExt, process::ExitStatus};
+use std::{fs::{self, File}, os::unix::fs::PermissionsExt, path::PathBuf, process::ExitStatus};
 
 use alumet::{
     pipeline::{
@@ -25,59 +25,12 @@ pub fn exec_child(external_command: String, args: Vec<String>) -> anyhow::Result
         Ok(val) => val,
         Err(e) => match e.kind() {
             std::io::ErrorKind::NotFound => {
-                let directory_entries_iter = fs::read_dir("./").unwrap();
-                let mut entry_list = String::from("Available files in the current directory:\n");
-                for entry_result in directory_entries_iter {
-                    let entry = entry_result.unwrap();
-                    let entry_type = entry.file_type().unwrap();
-                    if entry_type.is_file() {
-                        let entry_string = entry.file_name().into_string().unwrap();
-                        if external_command == entry_string {
-                            if let Ok(canonical_path) = fs::canonicalize(entry.path()) {
-                                if let Some(parent_path) = canonical_path.parent() {
-                                    log::info!("Found corresponding file in the current directory: {:?}", parent_path);
-                                } else {
-                                    log::info!("Found corresponding file in the current directory.");
-                                }
-                            } else {
-                                log::info!("Found corresponding file in the current directory.");
-                            }
-                            // Check for execution permissions
-                            let mut checkup_message =
-                                String::from(format!("Who can execute the file: {}:\n", external_command));
-                            if entry.metadata().unwrap().permissions().mode() & 0o100 != 0 {
-                                checkup_message = format!("{} - User: YES\n", checkup_message);
-                            } else {
-                                checkup_message = format!("{} - User: NO\n", checkup_message);
-                            }
-                            if entry.metadata().unwrap().permissions().mode() & 0o010 != 0 {
-                                checkup_message = format!("{} - Group: YES\n", checkup_message);
-                            } else {
-                                checkup_message = format!("{} - Group: NO\n", checkup_message);
-                            }
-                            if entry.metadata().unwrap().permissions().mode() & 0o001 != 0 {
-                                checkup_message = format!("{} - Others: YES\n", checkup_message);
-                            } else {
-                                checkup_message = format!("{} - Others: NO\n", checkup_message);
-                            }
-                            if !external_command.starts_with("./") {
-                                checkup_message =
-                                    format!("{}\n Please try again using the following syntax:\n", checkup_message);
-                                checkup_message = format!("{} [...] exec ./{}", checkup_message, external_command);
-                                for argument in &args {
-                                    checkup_message = format!("{} {}", checkup_message, argument)
-                                }
-                            }
-                            log::error!("{}", checkup_message);
-                            panic!("Maybe you could change the path to match with the correct one")
-                        } else {
-                            // Unable to find a corresponding things to execute
-                            entry_list = format!("{} -{}\n", entry_list, entry_string)
-                        }
-                    }
-                }
-                log::error!("Executable not found in the current directory, found:\n{}", entry_list);
-                panic!("Maybe you could change the path")
+                let return_error: String = handle_not_found(external_command, args);
+                panic!("{}", return_error);
+            },
+            std::io::ErrorKind::PermissionDenied => {
+                let return_error: String = handle_permission_denied(external_command);
+                panic!("{}", return_error);
             }
             _ => {
                 panic!("Error in child process");
@@ -94,6 +47,84 @@ pub fn exec_child(external_command: String, args: Vec<String>) -> anyhow::Result
     // Wait for the process to terminate.
     let status = p.wait().context("failed to wait for child process")?;
     Ok(status)
+}
+
+fn handle_permission_denied(external_command: String) -> String {
+    let file_permission_denied = match File::open(external_command.clone()) {
+        Ok(file) => {
+            file
+        },
+        Err(err) => {
+            panic!("Error when try to read the file: {}", err);
+        },
+    };
+    let metadata_file = file_permission_denied.metadata().expect(format!("Unable to retrieve metadata for: {}", external_command).as_str());
+    // Check for user permissions.
+    let user_perm = match metadata_file.permissions().mode() & 0o500 {
+        0 => "rx",
+        1 => "r",
+        4 => "x",
+        _ => "",
+    };
+    // Check for group permissions.
+    let group_perm: &str = match metadata_file.permissions().mode() & 0o050 {
+        0 => "rx",
+        1 => "r",
+        4 => "x",
+        _ => "",
+    };
+    // Check for other permissions.
+    let other_perm = match metadata_file.permissions().mode() & 0o005 {
+        0 => "rx",
+        1 => "r",
+        4 => "x",
+        _ => "",
+    };
+    if user_perm == "rx" || group_perm == "rx" || other_perm == "rx" {
+        log::error!("file '{}' is missing the following permissions:  'rx'", external_command);
+        log::info!("💡 Hint: try 'chmod +rx {}", external_command)
+    } else if user_perm == "r" || group_perm == "r" || other_perm == "r" {
+        log::error!("file '{}' is missing the following permissions:  'r'", external_command);
+        log::info!("💡 Hint: try 'chmod +r {}", external_command)
+    } else if user_perm == "x" || group_perm == "x" || other_perm == "x" {
+        log::error!("file '{}' is missing the following permissions:  'x'", external_command);
+        log::info!("💡 Hint: try 'chmod +x {}", external_command)
+    } else {
+        log::warn!("Can't determine right issue about the file: {}", external_command);
+    }
+    "Issue happened about file's permission".to_string()
+}
+
+fn handle_not_found(external_command: String, args: Vec<String>) -> String {
+    fn resolve_application_path() -> std::io::Result<PathBuf> {
+        std::env::current_exe()?.canonicalize()
+    }
+    log::error!("Command '{}' not found", external_command);
+    let directory_entries_iter = match fs::read_dir(".") {
+        Ok(directory) => {
+            directory
+        },
+        Err(err) => {
+            panic!("Error when try to read current directory: {}", err);
+        },
+    };
+    let app_path = resolve_application_path()
+                .ok()
+                .and_then(|p| p.to_str().map(|s| s.to_owned()))
+                .unwrap_or(String::from("path/to/agent"));
+    for entry_result in directory_entries_iter {
+        let entry = entry_result.unwrap();
+        let entry_type = entry.file_type().unwrap();
+        if entry_type.is_file() {
+            let entry_string = entry.file_name().into_string().unwrap();
+            if external_command == entry_string {
+                log::info!("💡Hint: A file named '{}' exists in the current directory. Prepend ./ to execute it.", entry_string);
+                log::info!("Example: {} exec ./{} {}",app_path, entry_string, args.join(" "));
+            }
+        }
+    }
+
+    "Issue happened because the file was not found".to_string()
 }
 
 // Triggers one measurement (on all sources that support manual trigger).
