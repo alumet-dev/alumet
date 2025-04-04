a5e34d57ed57555931c151cded60cfb5a618fc8c
Guillaume Raffin <theelectronwill@gmail.com>
2025-04-04 17:07:25 +0200

    test(plugin-nvidia): More complicated test for the jetson module

--- plugin-nvidia/src/jetson/ina.rs
+++ plugin-nvidia/src/jetson/ina.rs
@@ -10,7 +10,7 @@ use regex::{Match, Regex};
 use thiserror::Error;
 
 /// Detected INA sensor.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct InaSensor {
     /// Path to the sysfs directory of the sensor.
     pub path: PathBuf,
@@ -22,7 +22,7 @@ pub struct InaSensor {
 }
 
 /// Detected INA channel.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct InaChannel {
     pub id: u32,
     pub label: Option<String>,
@@ -32,7 +32,7 @@ pub struct InaChannel {
 }
 
 /// Detected metric available in a channel.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct InaRailMetric {
     pub path: PathBuf,
     pub unit: PrefixedUnit,
@@ -60,6 +60,15 @@ pub fn detect_ina_sensors() -> anyhow::Result<Vec<InaSensor>> {
     }
 }
 
+impl InaSensor {
+    pub fn sort_channels(&mut self) {
+        for chan in &mut self.channels {
+            chan.metrics.sort_by_key(|m| m.name.clone());
+        }
+        self.channels.sort_by_key(|chan| chan.id);
+    }
+}
+
 #[derive(Debug, Error)]
 enum DetectionError {
     /// Failed to access the root path that should point to INA sensors.
@@ -82,25 +91,6 @@ impl From<std::io::Error> for DetectionError {
 const SYSFS_INA_OLD: &str = "/sys/bus/i2c/drivers/ina3221x";
 const SYSFS_INA: &str = "/sys/bus/i2c/drivers/ina3221";
 
-/// Tries to guess the measurement unit that corresponds to the given channel prefix.
-/// Returns `None` if the unit cannot be found.
-fn guess_channel_unit(prefix: &Option<Match>) -> Option<PrefixedUnit> {
-    let prefix = prefix.unwrap().as_str();
-    match prefix {
-        "curr" => Some(PrefixedUnit::milli(Unit::Ampere)),
-        "in" => Some(PrefixedUnit::milli(Unit::Volt)),
-        "crit" => Some(PrefixedUnit::milli(Unit::Ampere)),
-        _ if prefix.contains("volt") => Some(PrefixedUnit::milli(Unit::Volt)),
-        _ if prefix.contains("current") => Some(PrefixedUnit::milli(Unit::Ampere)),
-        _ if prefix.contains("power") => Some(PrefixedUnit::milli(Unit::Watt)),
-        _ if prefix.contains("shunt") => Some(PrefixedUnit::milli(Unit::Custom {
-            unique_name: "Ohm".to_string(),
-            display_name: "Ω".to_string(),
-        })),
-        _ => None,
-    }
-}
-
 /// Detect the available INA sensors, assuming that Nvidia Jetpack version >= 5.0 is installed.
 ///
 /// The standard `sys_ina` looks like `/sys/bus/i2c/drivers/ina3221`.
@@ -121,6 +111,30 @@ fn detect_hierarchy_modern<P: AsRef<Path>>(sys_ina: P) -> Result<Vec<InaSensor>,
         Err(anyhow!("no sensor found in {}", sensor_path.display()))
     }
 
+    /// Tries to guess the measurement unit that corresponds to the given channel prefix.
+    /// Returns `None` if the unit cannot be found.
+    fn guess_channel_unit(prefix: &Option<Match>, suffix: &Option<Match>) -> Option<PrefixedUnit> {
+        if prefix.is_none() {
+            return None;
+        }
+        if suffix.is_some_and(|suffix| suffix.as_str() == "enable") {
+            // this is not a channel measurement but a simple flag
+            return None;
+        }
+
+        let prefix = prefix.unwrap().as_str();
+        match prefix {
+            "curr" => Some(PrefixedUnit::milli(Unit::Ampere)),
+            "in" => Some(PrefixedUnit::milli(Unit::Volt)),
+            "crit" => Some(PrefixedUnit::milli(Unit::Ampere)),
+            "shunt" => Some(PrefixedUnit::milli(Unit::Custom {
+                unique_name: "Ohm".to_string(),
+                display_name: "Ω".to_string(),
+            })),
+            _ => None,
+        }
+    }
+
     fn is_label_file(prefix: &Option<Match>, suffix: &Option<Match>) -> anyhow::Result<bool> {
         let prefix = prefix.context("parsing failed: missing prefix")?.as_str();
         let suffix = suffix.context("parsing failed: missing suffix")?.as_str();
@@ -132,7 +146,7 @@ fn detect_hierarchy_modern<P: AsRef<Path>>(sys_ina: P) -> Result<Vec<InaSensor>,
     }
 
     let metric_filename_pattern =
-        Regex::new(r"(?<prefix>[a-zA-Z]+)(?<id>\d+)_(?<suffix>[a-zA-Z]+)").expect("regex should be valid");
+        Regex::new(r"(?<prefix>[a-zA-Z]+)(?<id>\d+)_(?<suffix>[a-zA-Z_-]+)").expect("regex should be valid");
 
     detect_hierarchy(
         sys_ina,
@@ -160,6 +174,21 @@ fn detect_hierarchy_old_v4<P: AsRef<Path>>(sys_ina: P) -> Result<Vec<InaSensor>,
             }
         }
         Err(anyhow!("no sensor found in {}", sensor_path.display()))
+    }
+
+    /// Tries to guess the measurement unit that corresponds to the given channel prefix.
+    /// Returns `None` if the unit cannot be found.
+    fn guess_channel_unit(prefix: &Option<Match>, _suffix: &Option<Match>) -> Option<PrefixedUnit> {
+        if prefix.is_none() {
+            return None;
+        }
+        let prefix = prefix.unwrap().as_str();
+        match prefix {
+            _ if prefix.contains("volt") => Some(PrefixedUnit::milli(Unit::Volt)),
+            _ if prefix.contains("current") => Some(PrefixedUnit::milli(Unit::Ampere)),
+            _ if prefix.contains("power") => Some(PrefixedUnit::milli(Unit::Watt)),
+            _ => None,
+        }
     }
 
     fn is_label_file(prefix: &Option<Match>, suffix: &Option<Match>) -> anyhow::Result<bool> {
@@ -193,7 +222,7 @@ fn detect_hierarchy<P: AsRef<Path>>(
     sys_ina: P,
     metric_filename_pattern: Regex,
     sensor_channels_dir: fn(sensor_path: &Path) -> anyhow::Result<PathBuf>,
-    guess_channel_unit: fn(prefix: &Option<Match>) -> Option<PrefixedUnit>,
+    guess_channel_unit: fn(prefix: &Option<Match>, suffix: &Option<Match>) -> Option<PrefixedUnit>,
     is_label_file: fn(prefix: &Option<Match>, suffix: &Option<Match>) -> anyhow::Result<bool>,
     format_metric_name: fn(prefix: &Option<Match>, suffix: &Option<Match>) -> String,
 ) -> Result<Vec<InaSensor>, DetectionError> {
@@ -229,7 +258,7 @@ fn detect_hierarchy<P: AsRef<Path>>(
                     channel_labels.insert(channel_id, label);
                 } else {
                     // This file contains the (automatically updated) value of a metric.
-                    match guess_channel_unit(prefix) {
+                    match guess_channel_unit(prefix, suffix) {
                         Some(unit) => channel_metrics
                             .entry(channel_id)
                             .or_insert_with(|| Vec::with_capacity(5))
@@ -314,9 +343,11 @@ fn detect_hierarchy<P: AsRef<Path>>(
 mod tests {
     use std::collections::HashMap;
 
-    use crate::jetson::ina::DetectionError;
+    use crate::jetson::ina::{DetectionError, InaChannel, InaRailMetric, InaSensor};
 
     use super::{detect_hierarchy_modern, detect_hierarchy_old_v4};
+    use alumet::units::{PrefixedUnit, Unit};
+    use pretty_assertions::assert_eq;
     use tempfile::tempdir;
 
     #[test]
@@ -409,53 +440,168 @@ mod tests {
         std::fs::write(hwmon0.join("in0_label"), "Sensor 0, channel 0").unwrap();
         std::fs::write(hwmon0.join("curr0_input"), "0").unwrap();
         std::fs::write(hwmon0.join("in0_input"), "1").unwrap();
-        std::fs::write(hwmon0.join("curr0_crit"), "2").unwrap();
-        std::fs::write(hwmon0.join("crit0_max"), "3").unwrap();
+        let expected_s0_chan0 = InaChannel {
+            id: 0,
+            label: Some(String::from("Sensor 0, channel 0")),
+            metrics: vec![
+                InaRailMetric {
+                    path: hwmon0.join("curr0_input"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_input"),
+                },
+                InaRailMetric {
+                    path: hwmon0.join("in0_input"),
+                    unit: PrefixedUnit::milli(Unit::Volt),
+                    name: String::from("in_input"),
+                },
+            ],
+            description: None,
+        };
 
         std::fs::write(hwmon0.join("in1_label"), "Sensor 0, channel 1").unwrap();
         std::fs::write(hwmon0.join("curr1_input"), "10").unwrap();
         std::fs::write(hwmon0.join("in1_input"), "11").unwrap();
         std::fs::write(hwmon0.join("curr1_crit"), "12").unwrap();
         std::fs::write(hwmon0.join("crit1_max"), "13").unwrap();
+        let expected_s0_chan1 = InaChannel {
+            id: 1,
+            label: Some(String::from("Sensor 0, channel 1")),
+            metrics: vec![
+                InaRailMetric {
+                    path: hwmon0.join("curr1_input"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_input"),
+                },
+                InaRailMetric {
+                    path: hwmon0.join("in1_input"),
+                    unit: PrefixedUnit::milli(Unit::Volt),
+                    name: String::from("in_input"),
+                },
+                InaRailMetric {
+                    path: hwmon0.join("curr1_crit"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_crit"),
+                },
+                InaRailMetric {
+                    path: hwmon0.join("crit1_max"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("crit_max"),
+                },
+            ],
+            description: None,
+        };
 
         std::fs::write(hwmon1.join("in0_label"), "Sensor 1, channel 0").unwrap();
         std::fs::write(hwmon1.join("curr0_input"), "100").unwrap();
         std::fs::write(hwmon1.join("in0_input"), "101").unwrap();
         std::fs::write(hwmon1.join("curr0_crit"), "102").unwrap();
-        std::fs::write(hwmon1.join("crit0_max"), "103").unwrap();
+        std::fs::write(hwmon1.join("curr0_max"), "103").unwrap();
+        let expected_s1_chan0 = InaChannel {
+            id: 0,
+            label: Some(String::from("Sensor 1, channel 0")),
+            metrics: vec![
+                InaRailMetric {
+                    path: hwmon1.join("curr0_input"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_input"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("curr0_crit"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_crit"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("curr0_max"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_max"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("in0_input"),
+                    unit: PrefixedUnit::milli(Unit::Volt),
+                    name: String::from("in_input"),
+                },
+            ],
+            description: None,
+        };
+
+        // no label for channel 5
+        std::fs::write(hwmon1.join("curr5_input"), "100").unwrap();
+        std::fs::write(hwmon1.join("curr5_max"), "100").unwrap();
+        std::fs::write(hwmon1.join("curr5_max_alarm"), "100").unwrap();
+        std::fs::write(hwmon1.join("curr5_crit"), "100").unwrap();
+        std::fs::write(hwmon1.join("curr5_crit_alarm"), "100").unwrap();
+        std::fs::write(hwmon1.join("in5_input"), "100").unwrap();
+        std::fs::write(hwmon1.join("in5_enable"), "100").unwrap();
+        std::fs::write(hwmon1.join("shunt5_resistor"), "100").unwrap();
+        // some files that don't contain any metric
+        std::fs::write(hwmon1.join("power"), "123456789").unwrap();
+        std::fs::write(hwmon1.join("uevent"), "").unwrap();
+        std::fs::write(hwmon1.join("samples"), "").unwrap();
+        let expected_s1_chan5 = InaChannel {
+            id: 5,
+            label: None,
+            metrics: vec![
+                InaRailMetric {
+                    path: hwmon1.join("curr5_input"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_input"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("curr5_crit"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_crit"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("curr5_crit_alarm"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_crit_alarm"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("curr5_max"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_max"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("curr5_max_alarm"),
+                    unit: PrefixedUnit::milli(Unit::Ampere),
+                    name: String::from("curr_max_alarm"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("in5_input"),
+                    unit: PrefixedUnit::milli(Unit::Volt),
+                    name: String::from("in_input"),
+                },
+                InaRailMetric {
+                    path: hwmon1.join("shunt5_resistor"),
+                    unit: PrefixedUnit::milli(Unit::Custom {
+                        unique_name: String::from("Ohm"),
+                        display_name: String::from("Ω"),
+                    }),
+                    name: String::from("shunt_resistor"),
+                },
+            ],
+            description: None,
+        };
+
+        // Build what we expect
+        let mut expected_sensors = vec![
+            InaSensor {
+                path: sensor_0040,
+                i2c_id: String::from("1-0040"),
+                channels: vec![expected_s0_chan0, expected_s0_chan1],
+            },
+            InaSensor {
+                path: sensor_0041,
+                i2c_id: String::from("1-0041"),
+                channels: vec![expected_s1_chan0, expected_s1_chan5],
+            },
+        ];
 
         // Test the detection
-        let sensors = detect_hierarchy_modern(root).expect("detection failed");
-        println!("detected: {sensors:?}");
-
-        let mut sensor_ids: Vec<&str> = sensors.iter().map(|s| s.i2c_id.as_ref()).collect();
-        sensor_ids.sort();
-        assert_eq!(sensor_ids, vec!["1-0040", "1-0041"]);
-
-        let expected_channel_labels: HashMap<&str, Vec<&str>> = HashMap::from_iter(vec![
-            ("1-0040", vec!["Sensor 0, channel 0", "Sensor 0, channel 1"]),
-            ("1-0041", vec!["Sensor 1, channel 0"]),
-        ]);
-        let mut expected_metrics = vec!["in_input", "curr_input", "curr_crit", "crit_max"];
-        expected_metrics.sort();
-
-        for sensor in sensors.into_iter() {
-            let mut channel_labels: Vec<String> = sensor
-                .channels
-                .iter()
-                .map(|chan| chan.label.to_owned().unwrap_or_else(|| format!("channel_{}", chan.id)))
-                .collect();
-            channel_labels.sort();
-
-            let expected_labels = &expected_channel_labels[sensor.i2c_id.as_str()];
-            assert_eq!(expected_labels, &channel_labels);
-
-            for channel in sensor.channels {
-                let mut metrics: Vec<&String> = channel.metrics.iter().map(|m| &m.name).collect();
-                metrics.sort();
-                assert_eq!(metrics, expected_metrics);
-            }
-        }
+        let mut sensors = detect_hierarchy_modern(root).expect("detection failed");
+        sort_sensors_recursively(&mut expected_sensors);
+        sort_sensors_recursively(&mut sensors);
+        assert_eq!(expected_sensors, sensors);
     }
 
     #[test]
@@ -538,5 +684,12 @@ mod tests {
         let res_old = detect_hierarchy_old_v4(&root);
         assert!(matches!(res_modern, Err(DetectionError::SysfsAccess(_))));
         assert!(matches!(res_old, Err(DetectionError::SysfsAccess(_))));
+    }
+
+    fn sort_sensors_recursively(sensors: &mut Vec<InaSensor>) {
+        for s in sensors.iter_mut() {
+            s.sort_channels();
+        }
+        sensors.sort_by_key(|s| s.i2c_id.clone());
     }
 }
