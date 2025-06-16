2905cf22d0f9dda2a94218661a9f67be82ca7d6c
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@eviden.com>
2025-06-16 11:48:19 +0200

    fix(plugin-grace-hopper): better tests and change in the function to compute energy

--- plugin-grace-hopper/tests/tests.rs
+++ plugin-grace-hopper/tests/tests.rs
@@ -4,11 +4,10 @@ use alumet::{
     pipeline::naming::SourceName,
     plugin::PluginMetadata,
     test::{RuntimeExpectations, StartupExpectations},
-    units::PrefixedUnit,
 };
 use plugin_grace_hopper::{Config, GraceHopperPlugin};
-use std::io::Write;
 use std::{fs::File, time::Duration};
+use std::{io::Write, thread};
 use tempfile::tempdir;
 
 const TIMEOUT: Duration = Duration::from_secs(5);
@@ -73,16 +72,18 @@ fn test_correct_plugin_init_with_one_source_empty_value() {
         .expect_metric::<f64>("consumption", alumet::units::Unit::Joule)
         .expect_source("grace-hopper", "Module_0");
 
-    let runtime_expectation = RuntimeExpectations::new().test_source(
-        SourceName::from_str("grace-hopper", "Module_0"),
-        || {},
-        |m| {
-            assert_eq!(m.len(), 1);
-            for elm in m {
-                assert!(elm.value == WrappedMeasurementValue::F64(0.0));
-            }
-        },
-    );
+    let runtime_expectation = RuntimeExpectations::new()
+        .test_source(SourceName::from_str("grace-hopper", "Module_0"), || {}, |_m| {})
+        .test_source(
+            SourceName::from_str("grace-hopper", "Module_0"),
+            || {},
+            |m| {
+                assert_eq!(m.len(), 1);
+                for elm in m {
+                    assert!(elm.value == WrappedMeasurementValue::F64(0.0));
+                }
+            },
+        );
 
     let agent = agent::Builder::new(plugins)
         .with_expectations(startup_expectation)
@@ -107,7 +108,7 @@ fn test_correct_plugin_init_with_several_sources() {
     let mut file_avg = File::create(&file_path_average).unwrap();
     let mut file_int = File::create(&file_path_interval).unwrap();
     writeln!(file, "Module Power Socket 0").unwrap();
-    writeln!(file_avg, "123456789").unwrap();
+    writeln!(file_avg, "60").unwrap();
     writeln!(file_int, "50").unwrap();
 
     let file_path_info = root.path().join("hwmon2/device/power1_oem_info");
@@ -118,7 +119,7 @@ fn test_correct_plugin_init_with_several_sources() {
     let mut file_avg = File::create(&file_path_average).unwrap();
     let mut _file_int = File::create(&file_path_interval).unwrap();
     writeln!(file, "Grace Power Socket 0").unwrap();
-    writeln!(file_avg, "987654321").unwrap();
+    writeln!(file_avg, "62").unwrap();
 
     let file_path_info = root.path().join("hwmon3/device/power1_oem_info");
     let file_path_average = root.path().join("hwmon3/device/power1_average");
@@ -128,7 +129,7 @@ fn test_correct_plugin_init_with_several_sources() {
     let mut file_avg = File::create(&file_path_average).unwrap();
     let mut _file_int = File::create(&file_path_interval).unwrap();
     writeln!(file, "CPU Power Socket 2").unwrap();
-    writeln!(file_avg, "1234598761").unwrap();
+    writeln!(file_avg, "64").unwrap();
 
     let file_path_info = root.path().join("hwmon6/device/power1_oem_info");
     let file_path_average = root.path().join("hwmon6/device/power1_average");
@@ -138,8 +139,8 @@ fn test_correct_plugin_init_with_several_sources() {
     let mut file_avg = File::create(&file_path_average).unwrap();
     let mut file_int = File::create(&file_path_interval).unwrap();
     writeln!(file, "SysIO Power Socket 2").unwrap();
-    writeln!(file_avg, "678954321").unwrap();
-    writeln!(file_int, "50").unwrap();
+    writeln!(file_avg, "67").unwrap();
+    writeln!(file_int, "77").unwrap();
 
     let mut plugins = PluginSet::new();
     let config = Config {
@@ -163,41 +164,93 @@ fn test_correct_plugin_init_with_several_sources() {
     let runtime_expectation = RuntimeExpectations::new()
         .test_source(
             SourceName::from_str("grace-hopper", "Module_0"),
+            || {
+                thread::sleep(Duration::from_secs(1));
+            },
+            |_m| {},
+        )
+        .test_source(
+            SourceName::from_str("grace-hopper", "Module_0"),
             || {},
             |m| {
-                assert_eq!(m.len(), 1);
                 for elm in m {
-                    assert!(elm.value == WrappedMeasurementValue::F64(123456789.0));
+                    match elm.value {
+                        WrappedMeasurementValue::F64(value) => {
+                            assert!(value >= 60.0 && value <= 61.0);
+                        }
+                        WrappedMeasurementValue::U64(_) => {
+                            assert!(false);
+                        }
+                    }
                 }
             },
         )
         .test_source(
             SourceName::from_str("grace-hopper", "Grace_0"),
+            || {
+                thread::sleep(Duration::from_secs(1));
+            },
+            |_m| {},
+        )
+        .test_source(
+            SourceName::from_str("grace-hopper", "Grace_0"),
             || {},
             |m| {
-                assert_eq!(m.len(), 1);
                 for elm in m {
-                    assert!(elm.value == WrappedMeasurementValue::F64(987654321.0));
+                    match elm.value {
+                        WrappedMeasurementValue::F64(value) => {
+                            assert!(value >= 62.0 && value <= 63.0);
+                        }
+                        WrappedMeasurementValue::U64(_) => {
+                            assert!(false);
+                        }
+                    }
                 }
             },
         )
         .test_source(
             SourceName::from_str("grace-hopper", "CPU_2"),
+            || {
+                thread::sleep(Duration::from_secs(1));
+            },
+            |_m| {},
+        )
+        .test_source(
+            SourceName::from_str("grace-hopper", "CPU_2"),
             || {},
             |m| {
-                assert_eq!(m.len(), 1);
                 for elm in m {
-                    assert!(elm.value == WrappedMeasurementValue::F64(1234598761.0));
+                    match elm.value {
+                        WrappedMeasurementValue::F64(value) => {
+                            assert!(value >= 64.0 && value <= 65.0);
+                        }
+                        WrappedMeasurementValue::U64(_) => {
+                            assert!(false);
+                        }
+                    }
                 }
             },
         )
         .test_source(
             SourceName::from_str("grace-hopper", "SysIO_2"),
+            || {
+                thread::sleep(Duration::from_secs(1));
+            },
+            |_m| {},
+        )
+        .test_source(
+            SourceName::from_str("grace-hopper", "SysIO_2"),
             || {},
             |m| {
-                assert_eq!(m.len(), 1);
                 for elm in m {
-                    assert!(elm.value == WrappedMeasurementValue::F64(678954321.0));
+                    match elm.value {
+                        WrappedMeasurementValue::F64(value) => {
+                            assert!(value >= 67.0 && value <= 68.0);
+                        }
+                        WrappedMeasurementValue::U64(_) => {
+                            assert!(false);
+                        }
+                    }
                 }
             },
         );
