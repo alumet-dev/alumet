fdb4b950e7f1dc866ca980b2e1ea2337f6b6d10a
Guillaume Raffin <theelectronwill@gmail.com>
2025-07-03 16:23:21 +0200

    feat(plugin-cgroup/oar): serialize "involved_jobs" attribute

--- plugin-cgroup/src/plugins/oar/transform.rs
+++ plugin-cgroup/src/plugins/oar/transform.rs
@@ -25,20 +25,13 @@ impl JobInfoAttacher {
 impl Transform for JobInfoAttacher {
     fn apply(&mut self, measurements: &mut MeasurementBuffer, _ctx: &TransformContext) -> Result<(), TransformError> {
         // lazily initialized
-        let current_job_list = LazyCell::new(|| {
-            self.tracker
-                .known_jobs_sorted()
-                .into_iter()
-                .map(|id| id.to_string())
-                .collect::<Vec<_>>()
-                .join(",")
-        });
+        let current_job_list = LazyCell::new(|| self.tracker.known_jobs_sorted().into_iter().collect::<Vec<_>>());
         for m in measurements.iter_mut() {
             if !m.attributes_keys().any(|k| k == "job_id") {
                 // This measurement is not job-specific, attach the list of running jobs.
+                // See issue #209.
                 let jobs_attr = current_job_list.clone();
-                m.add_attr("jobs_ids", AttributeValue::String(jobs_attr));
-                // TODO check the format of the attribute with bruno
+                m.add_attr("involved_jobs", AttributeValue::ListU64(jobs_attr));
             }
         }
         Ok(())
