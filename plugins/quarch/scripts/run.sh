909cbc7b25f3ce4ab3c9c6183634e72caf78cafd
mdacosta <dacostab@insa-toulouse.fr>
2025-08-25 15:58:51 +0200

    refactor(plugin-quarch): making a new version without using pyo3

---
+++ plugins/quarch/scripts/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+source scripts-configuration.txt
+
+if [ $# -lt 2 ]; then
+  echo "Usage: bash $0 HOSTNAME [PLUGIN1,PLUGIN2,...]"
+  exit 1
+fi
+
+HOSTNAME="$1"
+shift
+
+IFS=',' read -ra PLUGINS <<< "$1"
+shift
+
+# By default, add always "quarch" at the beginning
+PLUGIN_LIST="quarch"
+for p in "${PLUGINS[@]}"; do
+  [[ "$p" != "quarch" ]] && PLUGIN_LIST+=",${p}"
+done
+
+EXPERIMENT_START_TIME=$(date +%Y-%m-%d-%H-%M-%S)
+EXPERIMENT_DIRECTORY="${EXPERIMENT_RESULTS_DIRECTORY}/${EXPERIMENT_START_TIME}"
+
+echo "Do you want to keep the current config for the result directory?"
+echo "-----"
+ssh root@${HOSTNAME} "echo ${EXPERIMENT_DIRECTORY}"
+echo "-----"
+read -p "Use this config? [Y/n] " CONFIRM
+if [[ "$CONFIRM" =~ ^[Nn]$ ]]; then
+    read -p "Enter the path you want to use for the results: " CUSTOM_DIRECTORY
+    EXPERIMENT_DIRECTORY="${CUSTOM_DIRECTORY}"
+fi
+mkdir -p "$EXPERIMENT_DIRECTORY"
+ssh root@${HOSTNAME} "mkdir -p $EXPERIMENT_DIRECTORY"
+
+ssh root@${HOSTNAME} "alumet-agent config regen"
+echo "Do you want to keep the current config for alumet?"
+echo "-----"
+ssh root@${HOSTNAME} "cat ${DEFAULT_CONFIG}"
+echo "-----"
+read -p "Use this config? [Y/n] " CONFIRM
+if [[ "$CONFIRM" =~ ^[Nn]$ ]]; then
+    read -p "Enter the path to the config file to use: " CUSTOM_CONFIG
+    CONFIG_ARG="--config ${CUSTOM_CONFIG}"
+else
+    CONFIG_ARG="" 
+fi
+
+echo "PLUGIN_LIST: $PLUGIN_LIST"
+
+ssh root@${HOSTNAME} "source /root/venv-quarchpy/bin/activate && alumet-agent ${CONFIG_ARG} --plugins \"${PLUGIN_LIST}\" run"
+
+echo -e "\n\n Gathering experiment results..."
+scp -r root@${HOSTNAME}:${EXPERIMENT_DIRECTORY}/* ${EXPERIMENT_DIRECTORY}/
+echo -e " Done.\n"
