89aad3b9b5d9df133b5547044c421a4dbc4247ab
Victory, Eagle <victoryeagle77@protonmail.com>
2025-11-20 16:16:57 +0100

    feat(amd-gpu): AMD GPU plugin integration + tests

---
+++ plugins/amd-gpu/build.rs
@@ -0,0 +1,19 @@
+use std::{env, path::PathBuf};
+
+fn main() {
+    if env::var("DOCS_RS").is_ok() {
+        return;
+    }
+
+    let bindings = bindgen::Builder::default()
+        .headers(["./include/amdsmi.h", "./include/amd_smiConfig.h"])
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .dynamic_library_name("libamd_smi")
+        .generate()
+        .unwrap();
+
+    let out_path = PathBuf::from("./src");
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
