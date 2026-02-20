cae4dfc8f4e50640f5c59e59c3ee60265765233f
valentin bouchentouf-cabot <valentin.bouchentouf-cabot@eviden.com>
2026-02-20 11:39:15 +0100

    fix lib call in lib.rs

--- plugins/energy-to-carbon/src/lib.rs
+++ plugins/energy-to-carbon/src/lib.rs
@@ -1,4 +1,21 @@
-use alumet::plugin::{rust::AlumetPlugin, AlumetPluginStart, ConfigTable};
+use std::time::Duration;
+use alumet::{
+    measurement::{MeasurementAccumulator, MeasurementPoint, Timestamp},
+    metrics::TypedMetricId,
+    pipeline::{
+        Source,
+        elements::{
+            error::PollError,
+            source::trigger,
+        },
+    },
+    plugin::{
+        AlumetPluginStart, ConfigTable,
+        rust::AlumetPlugin,
+    },
+    resources::{Resource, ResourceConsumer},
+    units::Unit,
+};
 
 pub struct EnergyToCarbonPlugin;
 
@@ -15,17 +32,59 @@ impl AlumetPlugin for EnergyToCarbonPlugin {
         Ok(None) // no config for the moment
     }
 
-    fn init(config: ConfigTable) -> anyhow::Result<Box<Self>> {
+    fn init(_config: ConfigTable) -> anyhow::Result<Box<Self>> {
         Ok(Box::new(EnergyToCarbonPlugin))
     }
 
     fn start(&mut self, alumet: &mut AlumetPluginStart) -> anyhow::Result<()> {
         log::info!("Hello!");
+
+        // create a metric for the source
+        let counter_metric = alumet.create_metric::<u64> (
+            "exemple_source_call_counter",
+            Unit::Unity,
+            "number of times the example source has been called",
+        )?;
+
+        // create the sources
+        let source = ExampleSource {
+            metric: counter_metric,
+            counter: 0,
+        };
+
+        // How the source is triggered
+        let trigger = trigger::builder::time_interval(Duration::from_secs(1)).build()?;
+
+        // Add the source to the measurement pipeline
+        alumet.add_source("counter", Box::new(source), trigger);
+
         Ok(())
     }
 
     fn stop(&mut self) -> anyhow::Result<()> {
         log::info!("Bye!");
+        Ok(())
+    }
+}
+
+struct ExampleSource {
+    metric: TypedMetricId<u64>,
+    counter: u64,
+}
+
+impl Source for ExampleSource {
+    fn poll(&mut self, acc: &mut MeasurementAccumulator, timestamp: Timestamp) -> Result<(), PollError> {
+        let n_calls = self.counter;
+        self.counter += 1;
+
+        let point = MeasurementPoint::new(
+            timestamp,
+            self.metric,
+            Resource::LocalMachine,
+            ResourceConsumer::LocalMachine,
+            n_calls,  // Measured value
+        );
+        acc.push(point);
         Ok(())
     }
 }
