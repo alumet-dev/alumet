043935ee30eb6a25e979876f96e8717dde69b037
valentin bouchentouf-cabot <valentin.bouchentouf-cabot@eviden.com>
2026-03-13 11:34:19 +0100

    feat: add exemple sources

--- plugins/energy-to-carbon/README.md
+++ plugins/energy-to-carbon/README.md
@@ -1,4 +1,4 @@
-# Energy to Carbon plugin <!-- omit in toc -->
+# Energy to Carbon <!-- omit in toc -->
 
 ## Table of Contents <!-- omit in toc -->
 
@@ -10,18 +10,22 @@
 
 ## Introduction
 
-This plugin estimate ...
+This plugin estimate...
 
 ```math
-\Large Energy=\frac{cgroupv2*cpu\_total\_usage*nb_{\text{vcpu}}*TDP}{10^6*pooling\_interval*nb_{\text{cpu}}}
+\Large {Emission} = {Energy} \times {Emission\_Intensity}
 ```
 
-- $`{cgroupv2*cpu\_total\_usage}`$: Total usage of CPU in micro seconds for a pod.
-- $`nb_{\text{vcpu}}`$: Number of virtual CPU of the hosting machine where pod is running.
-- $`nb_{\text{cpu}}`$: Number of physical CPU of the hosting machine where pod is running
-- $`{polling\_interval}`$: Polling interval of cgroupv2 input plugin.
+- $`{Emission}`$ **(gCO₂)**: Carbon footprint of the pod.
+- $`{Energy}`$ **(kWh)**: Energy consumed by the pod.
+- $`{Emission\_intensity}`$ **(gCO₂/kWh)**: CO₂ emission factor of the energy source.
+
 
 ## Energy estimation tdp plugin
+
+### Prepare your environment
+
+To work this plugin needs k8s plugin configured, so the needed things are related to k8s plugin requirements:
 
 ### How to use
 
@@ -33,25 +37,30 @@ cargo run
 
 The binary created by the compilation will be found under the target repository.
 
-### Prepare your environment
 
-To work this plugin needs k8s plugin configured, so the needed things are related to k8s plugin requirements:
+### How is $`{Emission\_intensity}`$ estimated ?
 
-1. cgroup v2
-2. kubectl
-3. alumet-reader user
+We use a cascading method, trying to use the most accurate method first and falling back to less precise ones if needed.
+
+``` Bash
+emission_intensity Cascade
+|
+├── 1. User-defined fixed value                  (custom, from own measurements)
+│
+├── 2. Country average from Country Code
+│
+└── 3. World average fallback                    (default, least accurate)
+```
+
+These methods have a parameter in the `config.toml` file. If not filled, they will default to None and be skipped. Here is an example:
+
+``` toml
+[plugins.energy-to-carbon]
+emission_intensity_override = 150
+country_code = "FRA"
+# the World average fallback default to 475 gCO₂/kWh
+```
 
 ### Configuration
 
-```toml
-[plugins.energy-estimation-tdp]
 ...
-```
-
-- `pool_interval`: ...
-
-To get the CPU capacity of a kubernetes node, execute the following command:
-
-```bash
-
-```
--- plugins/energy-to-carbon/src/lib.rs
+++ plugins/energy-to-carbon/src/lib.rs
@@ -25,66 +25,84 @@ impl AlumetPlugin for EnergyToCarbonPlugin {
     }
 
     fn version() -> &'static str {
+        log::info!("Version here!!!");
         env!("CARGO_PKG_VERSION") // gets the version from the Cargo.toml of the plugin crate
     }
 
     fn default_config() -> anyhow::Result<Option<ConfigTable>> {
+        log::info!("Default here!!!");
         Ok(None) // no config for the moment
     }
 
     fn init(_config: ConfigTable) -> anyhow::Result<Box<Self>> {
+        log::info!("Init here!!!");
         Ok(Box::new(EnergyToCarbonPlugin))
     }
 
     fn start(&mut self, alumet: &mut AlumetPluginStart) -> anyhow::Result<()> {
-        log::info!("Hello!");
+        log::info!("Start here!!");
 
         // create a metric for the source
-        let counter_metric = alumet.create_metric::<u64> (
-            "exemple_source_call_counter",
-            Unit::Unity,
-            "number of times the example source has been called",
+        let energy = alumet.create_metric::<u64> (
+            "exemple_energy",
+            Unit::Joule,
+            "42j sent every seconds (for testing only)",
         )?;
 
+        let not_energy = alumet.create_metric::<u64> (
+            "exemple_not_energy",
+            Unit::Second,
+            "2s sent every seconds (for testing only)",
+        )?;
+
+
         // create the sources
-        let source = ExampleSource {
-            metric: counter_metric,
-            counter: 0,
+        let source_energy = ExampleSource {
+            metric_energy: energy,
+            metric_not_energy: not_energy,
         };
 
         // How the source is triggered
-        let trigger = trigger::builder::time_interval(Duration::from_secs(1)).build()?;
+        let trigger_s = trigger::builder::time_interval(Duration::from_secs(1)).build()?;
 
         // Add the source to the measurement pipeline
-        alumet.add_source("counter", Box::new(source), trigger);
+        alumet.add_source("counter", Box::new(source_energy), trigger_s);
 
         Ok(())
     }
 
     fn stop(&mut self) -> anyhow::Result<()> {
-        log::info!("Bye!");
+        log::info!("Bye!!");
         Ok(())
     }
 }
 
 struct ExampleSource {
-    metric: TypedMetricId<u64>,
-    counter: u64,
+    metric_energy: TypedMetricId<u64>,
+    metric_not_energy: TypedMetricId<u64>,
 }
-
+// For testing only
 impl Source for ExampleSource {
     fn poll(&mut self, acc: &mut MeasurementAccumulator, timestamp: Timestamp) -> Result<(), PollError> {
-        let n_calls = self.counter;
-        self.counter += 1;
+        log::info!("Poll !!");
 
-        let point = MeasurementPoint::new(
+        let point_energy = MeasurementPoint::new(
             timestamp,
-            self.metric,
+            self.metric_energy,
             Resource::LocalMachine,
             ResourceConsumer::LocalMachine,
-            n_calls,  // Measured value
+            42,  // Measured value
         );
-        acc.push(point);
+
+        let point_not_energy = MeasurementPoint::new(
+            timestamp,
+            self.metric_not_energy,
+            Resource::LocalMachine,
+            ResourceConsumer::LocalMachine,
+            2,  // Measured value
+        );
+        acc.push(point_energy);
+        acc.push(point_not_energy);
         Ok(())
     }
 }
