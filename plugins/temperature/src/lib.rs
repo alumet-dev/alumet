842e8ce40a76e0e2948d34aa3858cf8ad4f6e33e
Clement Mommessin <clement.mommessin@inria.fr>
2026-08-05 15:49:11 +0200

    Run cargo fmt and clippy

--- plugins/temperature/src/lib.rs
+++ plugins/temperature/src/lib.rs
@@ -2,23 +2,20 @@ use std::thread;
 use std::time::{Duration, Instant};
 
 use alumet::{
-        plugin::{
-            ConfigTable,
-            AlumetPluginStart,
-            rust::{AlumetPlugin, deserialize_config, serialize_config},
-        },
-        measurement::{MeasurementBuffer, MeasurementPoint, Timestamp},
-        units::Unit,
-        resources::{ResourceConsumer},
+    measurement::{MeasurementBuffer, MeasurementPoint, Timestamp},
+    plugin::{
+        AlumetPluginStart, ConfigTable,
+        rust::{AlumetPlugin, deserialize_config, serialize_config},
+    },
+    resources::ResourceConsumer,
+    units::Unit,
 };
 
 use serde::{Deserialize, Serialize};
 
 use lm_sensors::{Initializer, LMSensors};
 
-use crate::{
-    temperature_sensors::{SensorsFeature},
-};
+use crate::temperature_sensors::SensorsFeature;
 
 mod temperature_sensors;
 
@@ -54,7 +51,6 @@ impl Default for Config {
     }
 }
 
-
 impl AlumetPlugin for TemperaturePlugin {
     fn name() -> &'static str {
         "temperature"
@@ -79,7 +75,7 @@ impl AlumetPlugin for TemperaturePlugin {
         let lm_metric = alumet.create_metric::<f64>(
             "temperature_lm_sensor",
             Unit::DegreeCelsius,
-            "Temperature measured, as reported by lm_sensors."
+            "Temperature measured, as reported by lm_sensors.",
         )?;
 
         // Get the config parameters out of 'self', to be used in the thread
@@ -88,23 +84,26 @@ impl AlumetPlugin for TemperaturePlugin {
 
         // A flush of the measurement buffer will be done after `flush_rounds` measurements
         let flush_rounds = ((self.config.flush_interval.as_nanos() / poll_interval.as_nanos()) as usize).max(1);
-        
+
         // Create an autonomous source and add the source to the measurement pipeline
         alumet.add_autonomous_source_builder("temperature_autonomous_source", move |_ctx, cancel_token, tx| {
             // An new thread is needed because LMSensors and internal objects
             // are not Sync nor Send
             let handle = thread::spawn(move || {
                 // Initialise all the things related to lm_sensors
-                let lmsensors: LMSensors = Initializer::default().initialize().expect("Could not initialise LMSensors.");
-                
+                let lmsensors: LMSensors = Initializer::default()
+                    .initialize()
+                    .expect("Could not initialise LMSensors.");
+
                 // This plugin only works for Intel processors for the moment
                 // TODO: check the CPU vendor and stop if it is not Intel
                 // TODO: consider adding support for AMD processors
 
                 // Only getting temperature sensors from coretemp.
-                let sensors_feature_list: Vec<SensorsFeature> = temperature_sensors::get_coretemp_feature_list(&lmsensors, package_only);
+                let sensors_feature_list: Vec<SensorsFeature> =
+                    temperature_sensors::get_coretemp_feature_list(&lmsensors, package_only);
                 log::info!("Got {} temperature sensor features", sensors_feature_list.len());
-                
+
                 let mut buf = MeasurementBuffer::new();
                 let mut next_poll_time = Instant::now() + poll_interval;
                 let mut round = 0;
@@ -120,7 +119,7 @@ impl AlumetPlugin for TemperaturePlugin {
                                     lm_metric,
                                     feature.resource.clone(),
                                     ResourceConsumer::LocalMachine,
-                                    value
+                                    value,
                                 ));
                             }
                             Err(e) => {
@@ -128,7 +127,7 @@ impl AlumetPlugin for TemperaturePlugin {
                             }
                         }
                     }
-                    
+
                     round += 1;
                     if round == flush_rounds {
                         // Push and clear the buffer after at most the flush_interval period
--- plugins/temperature/src/temperature_sensors.rs
+++ plugins/temperature/src/temperature_sensors.rs
@@ -1,12 +1,8 @@
-use anyhow::{anyhow};
+use anyhow::anyhow;
 
-use lm_sensors::{
-    FeatureRef,
-    LMSensors,
-    SubFeatureRef,
-};
+use lm_sensors::{FeatureRef, LMSensors, SubFeatureRef};
 
-use alumet::{resources::{Resource}};
+use alumet::resources::Resource;
 
 pub struct SensorsFeature<'a> {
     pub label: String,
@@ -16,14 +12,16 @@ pub struct SensorsFeature<'a> {
 
 impl SensorsFeature<'_> {
     pub fn new(lm_feature: FeatureRef, coretemp_id: u32) -> anyhow::Result<SensorsFeature> {
-        let label = lm_feature.label().expect("Sensor feature name is not a valid UTF-8 string.");
+        let label = lm_feature
+            .label()
+            .expect("Sensor feature name is not a valid UTF-8 string.");
         let resource = resource_from_string(&label, coretemp_id)?; // Error handled by the caller
         let subfeature = lm_feature.sub_feature_by_kind(lm_sensors::value::Kind::TemperatureInput)?; // Error handled by the caller
 
         Ok(SensorsFeature {
             label,
             resource,
-            input_temperature_subfeature: subfeature
+            input_temperature_subfeature: subfeature,
         })
     }
 
@@ -32,22 +30,25 @@ impl SensorsFeature<'_> {
     }
 }
 
-fn resource_from_string(name: &String, coretemp_id: u32) -> anyhow::Result<Resource> {
+fn resource_from_string(name: &str, coretemp_id: u32) -> anyhow::Result<Resource> {
     let v: Vec<&str> = name.split_whitespace().collect();
 
     match v[0] {
         "Package" => {
             if let Ok(id) = v.get(2).expect("Expected Package id value").parse() {
-                return Ok(Resource::CpuPackage {id});
+                return Ok(Resource::CpuPackage { id });
             }
-        },
+        }
         "Core" => {
             if let Ok(id) = v.get(1).expect("Expected Core id value").parse::<u32>() {
                 // Need to attach the package id to the CPU core id
                 let custom_id = format!("{}_{}", coretemp_id, id);
-                return Ok(Resource::Custom{kind: std::borrow::Cow::Borrowed("CpuCore"), id: custom_id.into()})
+                return Ok(Resource::Custom {
+                    kind: std::borrow::Cow::Borrowed("CpuCore"),
+                    id: custom_id.into(),
+                });
             }
-        },
+        }
         _ => {}
     }
 
@@ -57,31 +58,37 @@ fn resource_from_string(name: &String, coretemp_id: u32) -> anyhow::Result<Resou
 
 pub fn get_coretemp_feature_list<'a>(lmsensors: &'a LMSensors, package_only: bool) -> Vec<SensorsFeature<'a>> {
     let mut sensors_feature_list: Vec<SensorsFeature> = vec![];
-    for chip in lmsensors.chip_iter(None)
-                         .filter(|x| x.name()
-                                      .expect("Chip name from LMSensors is not a valid UTF-8 string.")
-                                      .starts_with("coretemp")) {
+    for chip in lmsensors.chip_iter(None).filter(|x| {
+        x.name()
+            .expect("Chip name from LMSensors is not a valid UTF-8 string.")
+            .starts_with("coretemp")
+    }) {
         // We use filter and not find because there may exist multiple coretemp chips with the same name
         // (when there are two CPU packages for example)
 
         // The chip name is expected to be of the form 'coretemp-isa-0000'
         // So we retrieve the coretemp id from the suffix
-        let coretemp_id: u32 = chip.name()
-                                   .unwrap() // Unwrap is safe because of the expect call few lines above
-                                   .split("-")
-                                   .last()
-                                   .expect("Coretemp chip name expected to be of the form 'coretemp-isa-XXXX'.")
-                                   .parse()
-                                   .expect("Coretemp chip name should be suffixed by the coretemp id.");
+        let coretemp_id: u32 = chip
+            .name()
+            .unwrap() // Unwrap is safe because of the expect call few lines above
+            .split('-')
+            .last()
+            .expect("Coretemp chip name expected to be of the form 'coretemp-isa-XXXX'.")
+            .parse()
+            .expect("Coretemp chip name should be suffixed by the coretemp id.");
 
         sensors_feature_list.extend(
             chip.feature_iter()
                 // Filter by feature::Kind::Temperature just to be sure
                 .filter(|x| x.kind() == Some(lm_sensors::feature::Kind::Temperature))
                 // If package_only is set, keep the feature if it corresponds to a Package temperature
-                .filter(|x| !package_only || x.label().expect("Component name is not a valid UTF-8 string.").starts_with("Package"))
-                .map(|x| SensorsFeature::new(x, coretemp_id)
-                                         .expect("Could not create LMSensors feature."))
+                .filter(|x| {
+                    !package_only
+                        || x.label()
+                            .expect("Component name is not a valid UTF-8 string.")
+                            .starts_with("Package")
+                })
+                .map(|x| SensorsFeature::new(x, coretemp_id).expect("Could not create LMSensors feature.")),
         );
     }
 
